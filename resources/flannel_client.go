60653d3bb5d9faae6cd76c0e29fb5d0ac1258980
Tim Schindler <xh3b4sd@gmail.com>
2017-01-24 19:22:46 +0100

    properly create network bridge name

--- resources/flannel_client.go
+++ resources/flannel_client.go
@@ -277,16 +277,20 @@ func (f *flannelClient) GenerateResources() ([]runtime.Object, error) {
 							Command: []string{
 								"/bin/sh",
 								"-c",
-								"while [ ! -f /run/flannel/networks/${CUSTOMER_ID}.env ]; do echo 'Waiting for flannel network'; sleep 1; done; /tmp/k8s_network_bridge.sh create ${CUSTOMER_ID} br${CUSTOMER_ID} ${NETWORK_INTERFACE} ${HOST_SUBNET_RANGE}",
+								"while [ ! -f /run/flannel/networks/${CLUSTER_ID}.env ]; do echo 'Waiting for flannel network'; sleep 1; done; /tmp/k8s_network_bridge.sh create ${CLUSTER_ID} ${NETWORK_BRIDGE_NAME} ${NETWORK_INTERFACE} ${HOST_SUBNET_RANGE}",
 							},
 							Env: []apiv1.EnvVar{
 								{
-									Name:  "CUSTOMER_ID",
-									Value: f.Spec.Customer,
+									Name:  "CLUSTER_ID",
+									Value: f.Spec.ClusterId,
 								},
 								{
 									Name:  "HOST_SUBNET_RANGE",
 									Value: f.Spec.GiantnetesConfiguration.HostSubnetRange,
+								},
+								{
+									Name:  "NETWORK_BRIDGE_NAME",
+									Value: networkBridgeName(f.Spec.ClusterId),
 								},
 								{
 									Name:  "NETWORK_INTERFACE",
