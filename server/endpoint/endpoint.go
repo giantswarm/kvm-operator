8f0ed3ab089dcba3912259994ab404eda587740a
Tim Schindler <xh3b4sd@gmail.com>
2017-02-16 17:25:41 +0100

    added server packages

---
+++ server/endpoint/endpoint.go
@@ -0,0 +1,72 @@
+package endpoint
+
+import (
+	microerror "github.com/giantswarm/microkit/error"
+	micrologger "github.com/giantswarm/microkit/logger"
+
+	"github.com/giantswarm/kvm-operator/server/endpoint/healthz"
+	"github.com/giantswarm/kvm-operator/server/endpoint/version"
+	"github.com/giantswarm/kvm-operator/server/middleware"
+	"github.com/giantswarm/kvm-operator/service"
+)
+
+// Config represents the configuration used to create a endpoint.
+type Config struct {
+	// Dependencies.
+	Logger     micrologger.Logger
+	Middleware *middleware.Middleware
+	Service    *service.Service
+}
+
+// DefaultConfig provides a default configuration to create a new endpoint by
+// best effort.
+func DefaultConfig() Config {
+	return Config{
+		// Dependencies.
+		Logger:     nil,
+		Middleware: nil,
+		Service:    nil,
+	}
+}
+
+// New creates a new configured endpoint.
+func New(config Config) (*Endpoint, error) {
+	var err error
+
+	var healthzEndpoint *healthz.Endpoint
+	{
+		healthzConfig := healthz.DefaultConfig()
+		healthzConfig.Logger = config.Logger
+		healthzConfig.Middleware = config.Middleware
+		healthzConfig.Service = config.Service
+		healthzEndpoint, err = healthz.New(healthzConfig)
+		if err != nil {
+			return nil, microerror.MaskAny(err)
+		}
+	}
+
+	var versionEndpoint *version.Endpoint
+	{
+		versionConfig := version.DefaultConfig()
+		versionConfig.Logger = config.Logger
+		versionConfig.Middleware = config.Middleware
+		versionConfig.Service = config.Service
+		versionEndpoint, err = version.New(versionConfig)
+		if err != nil {
+			return nil, microerror.MaskAny(err)
+		}
+	}
+
+	newEndpoint := &Endpoint{
+		Healthz: healthzEndpoint,
+		Version: versionEndpoint,
+	}
+
+	return newEndpoint, nil
+}
+
+// Endpoint is the endpoint collection.
+type Endpoint struct {
+	Healthz *healthz.Endpoint
+	Version *version.Endpoint
+}
