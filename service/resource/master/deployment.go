fa8f11423710d6eb60d658bded86dc5b64640db8
Tim Schindler <xh3b4sd@gmail.com>
2017-05-19 18:01:54 +0200

    create a list of deployments for master and worker nodes

--- service/resource/master/deployment.go
+++ service/resource/master/deployment.go
@@ -12,7 +12,9 @@ import (
 	extensionsv1 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
 
-func (s *Service) newDeployment(obj interface{}) (*extensionsv1.Deployment, error) {
+func (s *Service) newDeployments(obj interface{}) ([]*extensionsv1.Deployment, error) {
+	var deployments []*extensionsv1.Deployment
+
 	customObject, ok := obj.(*kvmtpr.CustomObject)
 	if !ok {
 		return nil, microerror.MaskAnyf(wrongTypeError, "expected '%T', got '%T'", &kvmtpr.CustomObject{}, obj)
@@ -20,135 +22,143 @@ func (s *Service) newDeployment(obj interface{}) (*extensionsv1.Deployment, erro
 
 	privileged := true
 	replicas := int32(1)
-	masterNode := customObject.Spec.Cluster.Masters[0]
 
-	deployment := &extensionsv1.Deployment{
-		TypeMeta: apiunversioned.TypeMeta{
-			Kind:       "deployment",
-			APIVersion: "extensions/v1beta",
-		},
-		ObjectMeta: apiv1.ObjectMeta{
-			Name: "master",
-			Labels: map[string]string{
-				"cluster":  resource.ClusterID(*customObject),
-				"customer": resource.ClusterCustomer(*customObject),
-				"app":      "master",
+	for _, masterNode := range customObject.Spec.Cluster.Masters {
+		deployment := &extensionsv1.Deployment{
+			TypeMeta: apiunversioned.TypeMeta{
+				Kind:       "deployment",
+				APIVersion: "extensions/v1beta",
 			},
-		},
-		Spec: extensionsv1.DeploymentSpec{
-			Strategy: extensionsv1.DeploymentStrategy{
-				Type: extensionsv1.RecreateDeploymentStrategyType,
-			},
-			Replicas: &replicas,
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: apiv1.ObjectMeta{
-					GenerateName: "master",
-					Labels: map[string]string{
-						"cluster":  resource.ClusterID(*customObject),
-						"customer": resource.ClusterCustomer(*customObject),
-						"app":      "master",
-					},
+			ObjectMeta: apiv1.ObjectMeta{
+				Name: "master",
+				Labels: map[string]string{
+					"cluster":  resource.ClusterID(*customObject),
+					"customer": resource.ClusterCustomer(*customObject),
+					"app":      "master",
 				},
-				Spec: apiv1.PodSpec{
-					HostNetwork: true,
-					Volumes: []apiv1.Volume{
-						{
-							Name: "etcd-data",
-							VolumeSource: apiv1.VolumeSource{
-								HostPath: &apiv1.HostPathVolumeSource{
-									Path: filepath.Join("/home/core/", resource.ClusterID(*customObject), "-k8s-master-vm/"),
-								},
-							},
-						},
-						{
-							Name: "images",
-							VolumeSource: apiv1.VolumeSource{
-								HostPath: &apiv1.HostPathVolumeSource{
-									Path: "/home/core/images/",
-								},
-							},
-						},
-						{
-							Name: "rootfs",
-							VolumeSource: apiv1.VolumeSource{
-								HostPath: &apiv1.HostPathVolumeSource{
-									Path: filepath.Join("/home/core/vms/", resource.ClusterID(*customObject), "-k8s-master-vm/"),
-								},
-							},
-						},
-						{
-							Name: "flannel",
-							VolumeSource: apiv1.VolumeSource{
-								HostPath: &apiv1.HostPathVolumeSource{
-									Path: "/run/flannel",
-								},
-							},
+			},
+			Spec: extensionsv1.DeploymentSpec{
+				Strategy: extensionsv1.DeploymentStrategy{
+					Type: extensionsv1.RecreateDeploymentStrategyType,
+				},
+				Replicas: &replicas,
+				Template: apiv1.PodTemplateSpec{
+					ObjectMeta: apiv1.ObjectMeta{
+						GenerateName: "master",
+						Labels: map[string]string{
+							"cluster":  resource.ClusterID(*customObject),
+							"customer": resource.ClusterCustomer(*customObject),
+							"app":      "master",
 						},
 					},
-					Containers: []apiv1.Container{
-						{
-							Name:            "k8s-kvm",
-							Image:           customObject.Spec.KVM.K8sKVM.Docker.Image,
-							ImagePullPolicy: apiv1.PullIfNotPresent,
-							SecurityContext: &apiv1.SecurityContext{
-								Privileged: &privileged,
-							},
-							Args: []string{
-								"master",
-							},
-							Env: []apiv1.EnvVar{
-								{
-									Name:  "CORES",
-									Value: fmt.Sprintf("%d", masterNode.CPUs),
-								},
-								{
-									Name: "DISK",
-									// TODO this should be configured via clustertpr.Node
-									Value: "4G",
-								},
-								{
-									Name: "HOSTNAME",
-									ValueFrom: &apiv1.EnvVarSource{
-										FieldRef: &apiv1.ObjectFieldSelector{
-											APIVersion: "v1",
-											FieldPath:  "metadata.name",
+					Spec: apiv1.PodSpec{
+						HostNetwork: true,
+						Volumes: []apiv1.Volume{
+							{
+								Name: "cloud-config",
+								VolumeSource: apiv1.VolumeSource{
+									ConfigMap: &apiv1.ConfigMapVolumeSource{
+										LocalObjectReference: apiv1.LocalObjectReference{
+											Name: resource.ConfigMapName(*customObject, masterNode),
 										},
 									},
 								},
-								{
-									Name:  "NETWORK_BRIDGE_NAME",
-									Value: resource.NetworkBridgeName(resource.ClusterID(*customObject)),
-								},
-								{
-									Name:  "MEMORY",
-									Value: masterNode.Memory,
-								},
-								{
-									Name:  "ROLE",
-									Value: "master",
+							},
+							{
+								Name: "etcd-data",
+								VolumeSource: apiv1.VolumeSource{
+									HostPath: &apiv1.HostPathVolumeSource{
+										Path: filepath.Join("/home/core/", resource.ClusterID(*customObject), "-k8s-master-vm/"),
+									},
 								},
 							},
-							VolumeMounts: []apiv1.VolumeMount{
-								{
-									Name:      "images",
-									MountPath: "/usr/code/images/",
+							{
+								Name: "images",
+								VolumeSource: apiv1.VolumeSource{
+									HostPath: &apiv1.HostPathVolumeSource{
+										Path: "/home/core/images/",
+									},
 								},
-								{
-									Name:      "rootfs",
-									MountPath: "/usr/code/rootfs/",
+							},
+							{
+								Name: "rootfs",
+								VolumeSource: apiv1.VolumeSource{
+									HostPath: &apiv1.HostPathVolumeSource{
+										Path: filepath.Join("/home/core/vms/", resource.ClusterID(*customObject), "-k8s-master-vm/"),
+									},
 								},
-								{
-									Name:      "etcd-data",
-									MountPath: "/etc/kubernetes/data/etcd/",
+							},
+						},
+						Containers: []apiv1.Container{
+							{
+								Name:            "k8s-kvm",
+								Image:           customObject.Spec.KVM.K8sKVM.Docker.Image,
+								ImagePullPolicy: apiv1.PullIfNotPresent,
+								SecurityContext: &apiv1.SecurityContext{
+									Privileged: &privileged,
 								},
-								// TODO cloud config has to be written into "/usr/code/cloudconfig/openstack/latest/user_data".
+								Args: []string{
+									"master",
+								},
+								Env: []apiv1.EnvVar{
+									{
+										Name:  "CORES",
+										Value: fmt.Sprintf("%d", masterNode.CPUs),
+									},
+									{
+										Name: "DISK",
+										// TODO this should be configured via clustertpr.Node
+										Value: "4G",
+									},
+									{
+										Name: "HOSTNAME",
+										ValueFrom: &apiv1.EnvVarSource{
+											FieldRef: &apiv1.ObjectFieldSelector{
+												APIVersion: "v1",
+												FieldPath:  "metadata.name",
+											},
+										},
+									},
+									{
+										Name:  "NETWORK_BRIDGE_NAME",
+										Value: resource.NetworkBridgeName(resource.ClusterID(*customObject)),
+									},
+									{
+										Name:  "MEMORY",
+										Value: masterNode.Memory,
+									},
+									{
+										Name:  "ROLE",
+										Value: "master",
+									},
+								},
+								VolumeMounts: []apiv1.VolumeMount{
+									{
+										Name:      "cloud-config",
+										MountPath: "/usr/code/cloudconfig/openstack/latest/",
+									},
+									{
+										Name:      "etcd-data",
+										MountPath: "/etc/kubernetes/data/etcd/",
+									},
+									{
+										Name:      "images",
+										MountPath: "/usr/code/images/",
+									},
+									{
+										Name:      "rootfs",
+										MountPath: "/usr/code/rootfs/",
+									},
+								},
 							},
 						},
 					},
 				},
 			},
-		},
+		}
+
+		deployments = append(deployments, deployment)
 	}
 
-	return deployment, nil
+	return deployments, nil
 }
--- service/resource/worker/deployment.go
+++ service/resource/worker/deployment.go
@@ -3,8 +3,6 @@ package worker
 import (
 	"fmt"
 	"path/filepath"
-	"strconv"
-	"time"
 
 	"github.com/giantswarm/kvm-operator/service/resource"
 	"github.com/giantswarm/kvmtpr"
@@ -14,7 +12,9 @@ import (
 	extensionsv1 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
 
-func (s *Service) newDeployment(obj interface{}) (*extensionsv1.Deployment, error) {
+func (s *Service) newDeployments(obj interface{}) ([]*extensionsv1.Deployment, error) {
+	var deployments []*extensionsv1.Deployment
+
 	customObject, ok := obj.(*kvmtpr.CustomObject)
 	if !ok {
 		return nil, microerror.MaskAnyf(wrongTypeError, "expected '%T', got '%T'", &kvmtpr.CustomObject{}, obj)
@@ -22,116 +22,131 @@ func (s *Service) newDeployment(obj interface{}) (*extensionsv1.Deployment, erro
 
 	privileged := true
 	replicas := int32(1)
-	workerID := strconv.FormatInt(time.Now().UnixNano(), 64)
-	workerNode := customObject.Spec.Cluster.Workers[0]
 
-	deployment := &extensionsv1.Deployment{
-		TypeMeta: apiunversioned.TypeMeta{
-			Kind:       "deployment",
-			APIVersion: "extensions/v1beta",
-		},
-		ObjectMeta: apiv1.ObjectMeta{
-			Name: "worker",
-			Labels: map[string]string{
-				"cluster":  resource.ClusterID(*customObject),
-				"customer": resource.ClusterCustomer(*customObject),
-				"app":      "worker",
+	for _, workerNode := range customObject.Spec.Cluster.Workers {
+		deployment := &extensionsv1.Deployment{
+			TypeMeta: apiunversioned.TypeMeta{
+				Kind:       "deployment",
+				APIVersion: "extensions/v1beta",
 			},
-		},
-		Spec: extensionsv1.DeploymentSpec{
-			Strategy: extensionsv1.DeploymentStrategy{
-				Type: extensionsv1.RecreateDeploymentStrategyType,
-			},
-			Replicas: &replicas,
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: apiv1.ObjectMeta{
-					Name: "worker",
-					Labels: map[string]string{
-						"cluster":  resource.ClusterID(*customObject),
-						"customer": resource.ClusterCustomer(*customObject),
-						"app":      "worker",
-					},
+			ObjectMeta: apiv1.ObjectMeta{
+				Name: "worker",
+				Labels: map[string]string{
+					"cluster":  resource.ClusterID(*customObject),
+					"customer": resource.ClusterCustomer(*customObject),
+					"app":      "worker",
 				},
-				Spec: apiv1.PodSpec{
-					HostNetwork: true,
-					Volumes: []apiv1.Volume{
-						{
-							Name: "images",
-							VolumeSource: apiv1.VolumeSource{
-								HostPath: &apiv1.HostPathVolumeSource{
-									Path: "/home/core/images/",
-								},
-							},
-						},
-						{
-							Name: "rootfs",
-							VolumeSource: apiv1.VolumeSource{
-								HostPath: &apiv1.HostPathVolumeSource{
-									Path: filepath.Join("/home/core/vms", resource.ClusterID(*customObject), workerID),
-								},
-							},
+			},
+			Spec: extensionsv1.DeploymentSpec{
+				Strategy: extensionsv1.DeploymentStrategy{
+					Type: extensionsv1.RecreateDeploymentStrategyType,
+				},
+				Replicas: &replicas,
+				Template: apiv1.PodTemplateSpec{
+					ObjectMeta: apiv1.ObjectMeta{
+						Name: "worker",
+						Labels: map[string]string{
+							"cluster":  resource.ClusterID(*customObject),
+							"customer": resource.ClusterCustomer(*customObject),
+							"app":      "worker",
 						},
 					},
-					Containers: []apiv1.Container{
-						{
-							Name:            "k8s-kvm",
-							Image:           customObject.Spec.KVM.K8sKVM.Docker.Image,
-							ImagePullPolicy: apiv1.PullIfNotPresent,
-							SecurityContext: &apiv1.SecurityContext{
-								Privileged: &privileged,
-							},
-							Args: []string{
-								"worker",
-							},
-							Env: []apiv1.EnvVar{
-								{
-									Name:  "CORES",
-									Value: fmt.Sprintf("%d", workerNode.CPUs),
-								},
-								{
-									Name: "DISK",
-									// TODO this should be configured via clustertpr.Node
-									Value: "4G",
-								},
-								{
-									Name: "HOSTNAME",
-									ValueFrom: &apiv1.EnvVarSource{
-										FieldRef: &apiv1.ObjectFieldSelector{
-											APIVersion: "v1",
-											FieldPath:  "metadata.name",
+					Spec: apiv1.PodSpec{
+						HostNetwork: true,
+						Volumes: []apiv1.Volume{
+							{
+								Name: "cloud-config",
+								VolumeSource: apiv1.VolumeSource{
+									ConfigMap: &apiv1.ConfigMapVolumeSource{
+										LocalObjectReference: apiv1.LocalObjectReference{
+											Name: resource.ConfigMapName(*customObject, workerNode),
 										},
 									},
 								},
-								{
-									Name:  "NETWORK_BRIDGE_NAME",
-									Value: resource.NetworkBridgeName(resource.ClusterID(*customObject)),
-								},
-								{
-									Name:  "MEMORY",
-									Value: workerNode.Memory,
-								},
-								{
-									Name:  "ROLE",
-									Value: "worker",
+							},
+							{
+								Name: "images",
+								VolumeSource: apiv1.VolumeSource{
+									HostPath: &apiv1.HostPathVolumeSource{
+										Path: "/home/core/images/",
+									},
 								},
 							},
-							VolumeMounts: []apiv1.VolumeMount{
-								{
-									Name:      "images",
-									MountPath: "/usr/code/images/",
+							{
+								Name: "rootfs",
+								VolumeSource: apiv1.VolumeSource{
+									HostPath: &apiv1.HostPathVolumeSource{
+										Path: filepath.Join("/home/core/vms", resource.ClusterID(*customObject), workerNode.ID),
+									},
 								},
-								{
-									Name:      "rootfs",
-									MountPath: "/usr/code/rootfs/",
+							},
+						},
+						Containers: []apiv1.Container{
+							{
+								Name:            "k8s-kvm",
+								Image:           customObject.Spec.KVM.K8sKVM.Docker.Image,
+								ImagePullPolicy: apiv1.PullIfNotPresent,
+								SecurityContext: &apiv1.SecurityContext{
+									Privileged: &privileged,
 								},
-								// TODO cloud config has to be written into "/usr/code/cloudconfig/openstack/latest/user_data".
+								Args: []string{
+									"worker",
+								},
+								Env: []apiv1.EnvVar{
+									{
+										Name:  "CORES",
+										Value: fmt.Sprintf("%d", workerNode.CPUs),
+									},
+									{
+										Name: "DISK",
+										// TODO this should be configured via clustertpr.Node
+										Value: "4G",
+									},
+									{
+										Name: "HOSTNAME",
+										ValueFrom: &apiv1.EnvVarSource{
+											FieldRef: &apiv1.ObjectFieldSelector{
+												APIVersion: "v1",
+												FieldPath:  "metadata.name",
+											},
+										},
+									},
+									{
+										Name:  "NETWORK_BRIDGE_NAME",
+										Value: resource.NetworkBridgeName(resource.ClusterID(*customObject)),
+									},
+									{
+										Name:  "MEMORY",
+										Value: workerNode.Memory,
+									},
+									{
+										Name:  "ROLE",
+										Value: "worker",
+									},
+								},
+								VolumeMounts: []apiv1.VolumeMount{
+									{
+										Name:      "cloud-config",
+										MountPath: "/usr/code/cloudconfig/openstack/latest/",
+									},
+									{
+										Name:      "images",
+										MountPath: "/usr/code/images/",
+									},
+									{
+										Name:      "rootfs",
+										MountPath: "/usr/code/rootfs/",
+									},
+								},
 							},
 						},
 					},
 				},
 			},
-		},
+		}
+
+		deployments = append(deployments, deployment)
 	}
 
-	return deployment, nil
+	return deployments, nil
 }
