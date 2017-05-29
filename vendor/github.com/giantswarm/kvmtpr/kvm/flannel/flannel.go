b775a7e1ea6085b5a8e9fb2f86953c6242cc42ce
Tim Schindler <xh3b4sd@gmail.com>
2017-05-29 11:48:27 +0200

    updated vendor deps

--- vendor/github.com/giantswarm/kvmtpr/kvm/flannel/flannel.go
+++ vendor/github.com/giantswarm/kvmtpr/kvm/flannel/flannel.go
@@ -1,8 +1,8 @@
 package flannel
 
 import (
-	"github.com/giantswarm/clustertpr/flannel/client"
-	"github.com/giantswarm/clustertpr/flannel/docker"
+	"github.com/giantswarm/kvmtpr/kvm/flannel/client"
+	"github.com/giantswarm/kvmtpr/kvm/flannel/docker"
 )
 
 type Flannel struct {
@@ -16,6 +16,8 @@ type Flannel struct {
 	Interface string `json:"interface" yaml:"interface"`
 	// Network is the subnet specification, e.g. 10.0.9.0/16.
 	Network string `json:"network" yaml:"network"`
+	// PrivateNetwork is the private network to block against, e.g. 10.0.4.0/24.
+	PrivateNetwork string `json:"privateNetwork" yaml:"privateNetwork"`
 	// VNI is the vxlan network identifier, e.g. 9.
 	VNI int `json:"vni" yaml:"vni"`
 }
--- vendor/github.com/giantswarm/operatorkit/tpr/tpr.go
+++ vendor/github.com/giantswarm/operatorkit/tpr/tpr.go
@@ -1,7 +1,6 @@
 package tpr
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/cenkalti/backoff"
@@ -13,6 +12,10 @@ import (
 )
 
 const (
+	// ResyncPeriod is the interval at which the Informer cache is invalidated,
+	// and the lister function is called.
+	ResyncPeriod = 1 * time.Minute
+
 	tprInitMaxElapsedTime = 2 * time.Minute
 )
 
@@ -22,19 +25,16 @@ type Config struct {
 
 	// Settings.
 
-	// Name of the kind of ThirdPartyObjects. It should be in lower case
-	// and hyphen delimited. Kind names will be converted to CamelCase
-	// when creating ThirdPartyObjects. Hyphens in the kind are assumed to
-	// be word breaks. For instance the kind camel-case would be converted
-	// to CamelCase but camelcase would be converted to Camelcase.
+	// Name takes the form <kind>.<group> (Note: The group is also called
+	// a domain). You are expected to provide a unique kind and group name
+	// in order to avoid conflicts with other ThirdPartyResource objects.
+	// Kind names will be converted to CamelCase when creating instances of
+	// the ThirdPartyResource.  Hyphens in the kind are assumed to be word
+	// breaks. For instance the kind camel-case would be converted to
+	// CamelCase but camelcase would be converted to Camelcase.
 	Name string
 
-	// Domain of ThirdPartyResource, e.g. example.com. Along with Name must
-	// create an unique pair.
-	Domain string
-
-	// Version is API version, e.g. v1. When creating ThirdPartyObjects will be
-	// prefixed with Domain, e.g. example.com/v1.
+	// Version is TPR version, e.g. v1.
 	Version string
 
 	// Description is free text description.
@@ -47,13 +47,15 @@ type Config struct {
 type TPR struct {
 	clientset kubernetes.Interface
 
-	name          string
-	group         string
-	version       string
-	description   string
-	qualifiedName string // name.group
+	name        string // see Config.Name
+	kind        string // see Config.Name
+	group       string // see Config.Name
+	version     string
+	apiVersion  string // apiVersion is group/version
+	description string
 
-	endpointList string
+	// API for this TPR kind name.
+	resourceName string
 }
 
 func New(config Config) (*TPR, error) {
@@ -63,9 +65,6 @@ func New(config Config) (*TPR, error) {
 	if config.Name == "" {
 		return nil, microerror.MaskAnyf(invalidConfigError, "name must not be empty")
 	}
-	if config.Domain == "" {
-		return nil, microerror.MaskAnyf(invalidConfigError, "group must not be empty")
-	}
 	if config.Version == "" {
 		return nil, microerror.MaskAnyf(invalidConfigError, "version must not be empty")
 	}
@@ -73,18 +72,61 @@ func New(config Config) (*TPR, error) {
 		return nil, microerror.MaskAnyf(invalidConfigError, "description must not be empty")
 	}
 
+	kind, group, err := extractKindAndGroup(config.Name)
+	if err != nil {
+		return nil, microerror.MaskAny(err)
+	}
+
 	tpr := &TPR{
 		clientset: config.Clientset,
 
-		name:          config.Name,
-		group:         config.Domain,
-		version:       config.Version,
-		description:   config.Description,
-		qualifiedName: config.Name + "." + config.Domain,
+		name:        config.Name,
+		kind:        kind,
+		group:       group,
+		version:     config.Version,
+		apiVersion:  group + "/" + config.Version,
+		description: config.Description,
 
-		endpointList: fmt.Sprintf("/apis/%s/%s/%ss", config.Domain, config.Version, config.Name),
+		resourceName: unsafeGuessKindToResource(kind),
 	}
 	return tpr, nil
+}
+
+// Kind returns a TPR kind extracted from Name. Useful when creating
+// ThirdPartyObjects. See Config.Name godoc for details.
+func (t *TPR) Kind() string { return t.kind }
+
+// APIVersion returns a TPR APIVersion created from group and version. It takes
+// format <group>/<version>. Useful for creating ThirdPartyObjects. See
+// Config.Name and Config.Version for details.
+func (t *TPR) APIVersion() string { return t.apiVersion }
+
+// Name returns a TPR name provided with Config.Name.
+func (t *TPR) Name() string { return t.name }
+
+// Group returns a TPR group extracted from Name. See Config.Name godoc for details.
+func (t *TPR) Group() string { return t.group }
+
+// Endpoint returns a TPR resource endpoint registered in the Kubernetes API
+// under a given namespace. The default namespace will be used when the
+// argument is an empty string.
+func (t *TPR) Endpoint(namespace string) string {
+	nsResource := t.resourceName
+	if len(namespace) != 0 {
+		nsResource = "namespace/" + namespace + "/" + t.resourceName
+	}
+	return "/apis/" + t.group + "/" + t.version + "/" + nsResource
+}
+
+// Endpoint returns a TPR watch resource endpoint registered in the Kubernetes
+// API under a given namespace. The default namespace will be used when the
+// argument is an empty string.
+func (t *TPR) WatchEndpoint(namespace string) string {
+	nsResource := "watch/" + t.resourceName
+	if len(namespace) != 0 {
+		nsResource = "namespace/" + namespace + "/watch/" + t.resourceName
+	}
+	return "/apis/" + t.group + "/" + t.version + "/" + nsResource
 }
 
 // CreateAndWait creates a TPR and waits till it is initialized in the cluster.
@@ -100,11 +142,11 @@ func (t *TPR) CreateAndWait() error {
 func (t *TPR) CreateAndWaitBackOff(initBackOff backoff.BackOff) error {
 	err := t.create()
 	if err != nil {
-		return microerror.MaskAnyf(err, "creating TPR %s", t.qualifiedName)
+		return microerror.MaskAnyf(err, "creating TPR %s", t.name)
 	}
 	err = t.waitInit(initBackOff)
 	if err != nil {
-		return microerror.MaskAnyf(err, "waiting for TPR %s initialization", t.qualifiedName)
+		return microerror.MaskAnyf(err, "waiting for TPR %s initialization", t.name)
 	}
 	return nil
 }
@@ -114,7 +156,7 @@ func (t *TPR) CreateAndWaitBackOff(initBackOff backoff.BackOff) error {
 func (t *TPR) create() error {
 	tpr := &v1beta1.ThirdPartyResource{
 		ObjectMeta: v1.ObjectMeta{
-			Name: t.qualifiedName,
+			Name: t.name,
 		},
 		Versions: []v1beta1.APIVersion{
 			{Name: t.version},
@@ -124,14 +166,15 @@ func (t *TPR) create() error {
 
 	_, err := t.clientset.ExtensionsV1beta1().ThirdPartyResources().Create(tpr)
 	if err != nil && !errors.IsAlreadyExists(err) {
-		return microerror.MaskAnyf(err, "creating TPR %s", t.qualifiedName)
+		return microerror.MaskAnyf(err, "creating TPR %s", t.name)
 	}
 	return nil
 }
 
 func (t *TPR) waitInit(retry backoff.BackOff) error {
+	endpoint := t.Endpoint("")
 	op := func() error {
-		_, err := t.clientset.CoreV1().RESTClient().Get().RequestURI(t.endpointList).DoRaw()
+		_, err := t.clientset.CoreV1().RESTClient().Get().RequestURI(endpoint).DoRaw()
 		return err
 	}
 
@@ -140,5 +183,5 @@ func (t *TPR) waitInit(retry backoff.BackOff) error {
 	if errors.IsNotFound(err) {
 		err = tprInitTimeoutError
 	}
-	return microerror.MaskAnyf(err, "requesting TPR %s", t.qualifiedName)
+	return microerror.MaskAnyf(err, "requesting TPR %s", t.name)
 }
