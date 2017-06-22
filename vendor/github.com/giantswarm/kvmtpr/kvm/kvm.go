a0643e3e35bb5c7b6e1be831b570bbea4eff4de4
Tim Schindler <xh3b4sd@gmail.com>
2017-06-22 13:06:50 +0200

    updated vendor deps

--- vendor/github.com/giantswarm/kvmtpr/kvm/kvm.go
+++ vendor/github.com/giantswarm/kvmtpr/kvm/kvm.go
@@ -7,6 +7,7 @@ import (
 	"github.com/giantswarm/kvmtpr/kvm/k8skvm"
 	"github.com/giantswarm/kvmtpr/kvm/kubectl"
 	"github.com/giantswarm/kvmtpr/kvm/network"
+	"github.com/giantswarm/kvmtpr/kvm/node"
 )
 
 type KVM struct {
@@ -15,5 +16,7 @@ type KVM struct {
 	Flannel         flannel.Flannel                 `json:"flannel" yaml:"flannel"`
 	K8sKVM          k8skvm.K8sKVM                   `json:"k8sKVM" yaml:"k8sKVM"`
 	Kubectl         kubectl.Kubectl                 `json:"kubectl" yaml:"kubectl"`
+	Masters         []node.Node                     `json:"masters" yaml:"masters"`
 	Network         network.Network                 `json:"network" yaml:"network"`
+	Workers         []node.Node                     `json:"workers" yaml:"workers"`
 }
