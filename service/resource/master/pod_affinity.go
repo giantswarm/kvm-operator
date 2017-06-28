b1600a816b87399b1a87a8b502e698190e3bf3c6
Paweł Kopiczko <kopiczko@gmail.com>
2017-06-28 19:47:01 +0200

    use K8s 1.6 affinity (#112)

--- service/resource/master/pod_affinity.go
+++ service/resource/master/pod_affinity.go
@@ -5,18 +5,18 @@ import (
 	"github.com/giantswarm/kvmtpr"
 	microerror "github.com/giantswarm/microkit/error"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/pkg/api"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
 )
 
-func (s *Service) newPodAfinity(obj interface{}) (*api.Affinity, error) {
+func (s *Service) newPodAfinity(obj interface{}) (*apiv1.Affinity, error) {
 	customObject, ok := obj.(*kvmtpr.CustomObject)
 	if !ok {
 		return nil, microerror.MaskAnyf(wrongTypeError, "expected '%T', got '%T'", &kvmtpr.CustomObject{}, obj)
 	}
 
-	podAffinity := &api.Affinity{
-		PodAntiAffinity: &api.PodAntiAffinity{
-			RequiredDuringSchedulingIgnoredDuringExecution: []api.PodAffinityTerm{
+	podAffinity := &apiv1.Affinity{
+		PodAntiAffinity: &apiv1.PodAntiAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: []apiv1.PodAffinityTerm{
 				{
 					LabelSelector: &apismetav1.LabelSelector{
 						MatchExpressions: []apismetav1.LabelSelectorRequirement{
