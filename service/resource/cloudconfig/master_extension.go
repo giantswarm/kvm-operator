c4f3145810f3f7f754eaf30c1a6dffd0ba399633
Roman Sokolkov <roman@giantswarm.io>
2017-07-13 12:43:55 +0200

    Added etcd mounts for master
    https://github.com/giantswarm/kvm-operator/issues/121

--- service/resource/cloudconfig/master_extension.go
+++ service/resource/cloudconfig/master_extension.go
@@ -129,7 +129,56 @@ func (me *MasterExtension) Files() ([]k8scloudconfig.FileAsset, error) {
 }
 
 func (me *MasterExtension) Units() ([]k8scloudconfig.UnitAsset, error) {
-	return nil, nil
+	unitsMeta := []k8scloudconfig.UnitMetadata{
+		// Mount etcd volume when directory first accessed
+		// This automount is workaround for
+		// https://bugzilla.redhat.com/show_bug.cgi?id=1184122
+		k8scloudconfig.UnitMetadata{
+			AssetContent: `[Unit]
+Description=Automount for etcd volume
+[Automount]
+Where=/etc/kubernetes/data/etcd
+[Install]
+WantedBy=multi-user.target
+`,
+			Name:    "etc-kubernetes-data-etcd.automount",
+			Enable:  true,
+			Command: "start",
+		},
+		// Mount for etcd volume activated by automount
+		k8scloudconfig.UnitMetadata{
+			AssetContent: `[Unit]
+Description=Mount for etcd volume
+[Mount]
+What=etcdshare
+Where=/etc/kubernetes/data/etcd
+Options=trans=virtio,version=9p2000.L,cache=mmap
+Type=9p
+[Install]
+WantedBy=multi-user.target
+`,
+			Name:   "etc-kubernetes-data-etcd.mount",
+			Enable: false,
+		},
+	}
+
+	var newUnits []k8scloudconfig.UnitAsset
+
+	for _, fm := range unitsMeta {
+		c, err := k8scloudconfig.RenderAssetContent(fm.AssetContent, nil)
+		if err != nil {
+			return nil, microerror.MaskAny(err)
+		}
+
+		unitAsset := k8scloudconfig.UnitAsset{
+			Metadata: fm,
+			Content:  c,
+		}
+
+		newUnits = append(newUnits, unitAsset)
+	}
+
+	return newUnits, nil
 }
 
 func (me *MasterExtension) VerbatimSections() []k8scloudconfig.VerbatimSection {
