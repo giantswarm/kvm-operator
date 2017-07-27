42edcc7f63a28f2063e747ef7b94149a2e8376b5
Roman Sokolkov <roman@giantswarm.io>
2017-07-27 12:26:43 +0200

    Update vendor for new k8scloudconfig version

--- vendor/github.com/matttproud/golang_protobuf_extensions/pbtest/doc.go
+++ vendor/github.com/matttproud/golang_protobuf_extensions/pbtest/doc.go
@@ -1,9 +1,10 @@
-// Copyright 2016 The Prometheus Authors
+// Copyright 2015 Matt T. Proud
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -11,20 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package config
-
-import (
-	"fmt"
-	"strings"
-)
-
-func checkOverflow(m map[string]interface{}, ctx string) error {
-	if len(m) > 0 {
-		var keys []string
-		for k := range m {
-			keys = append(keys, k)
-		}
-		return fmt.Errorf("unknown fields in %s: %s", ctx, strings.Join(keys, ", "))
-	}
-	return nil
-}
+// Package pbtest provides mechanisms to assist with testing of Protocol Buffer messages.
+package pbtest
