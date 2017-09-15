1edbd57f939588e0442a43f3dd32136c27a1741a
Tim Schindler <xh3b4sd@gmail.com>
2017-09-15 18:43:47 +0200

    return nil types to not create it already exists

--- service/resource/configmap/resource.go
+++ service/resource/configmap/resource.go
@@ -188,7 +188,7 @@ func (r *Resource) GetDeleteState(ctx context.Context, obj, currentState, desire
 }
 
 func (r *Resource) GetUpdateState(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, interface{}, interface{}, error) {
-	return []*apiv1.ConfigMap{}, []*apiv1.ConfigMap{}, []*apiv1.ConfigMap{}, nil
+	return nil, nil, nil, nil
 }
 
 func (r *Resource) Name() string {
@@ -206,7 +206,7 @@ func (r *Resource) ProcessCreateState(ctx context.Context, obj, createState inte
 	}
 
 	// Create the config maps in the Kubernetes API.
-	if configMapsToCreate != nil {
+	if len(configMapsToCreate) != 0 {
 		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "creating the config maps in the Kubernetes API")
 
 		namespace := key.ClusterNamespace(customObject)
@@ -237,7 +237,7 @@ func (r *Resource) ProcessDeleteState(ctx context.Context, obj, deleteState inte
 		return microerror.Mask(err)
 	}
 
-	if configMapsToDelete != nil {
+	if len(configMapsToDelete) != 0 {
 		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "deleting the config maps in the Kubernetes API")
 
 		// Create the config maps in the Kubernetes API.
--- service/resource/service/resource.go
+++ service/resource/service/resource.go
@@ -179,7 +179,7 @@ func (r *Resource) GetDeleteState(ctx context.Context, obj, currentState, desire
 }
 
 func (r *Resource) GetUpdateState(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, interface{}, interface{}, error) {
-	return []*apiv1.Service{}, []*apiv1.Service{}, []*apiv1.Service{}, nil
+	return nil, nil, nil, nil
 }
 
 func (r *Resource) Name() string {
@@ -196,7 +196,7 @@ func (r *Resource) ProcessCreateState(ctx context.Context, obj, createState inte
 		return microerror.Mask(err)
 	}
 
-	if servicesToCreate != nil {
+	if len(servicesToCreate) != 0 {
 		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "creating the services in the Kubernetes API")
 
 		namespace := key.ClusterNamespace(customObject)
@@ -227,7 +227,7 @@ func (r *Resource) ProcessDeleteState(ctx context.Context, obj, deleteState inte
 		return microerror.Mask(err)
 	}
 
-	if servicesToDelete != nil {
+	if len(servicesToDelete) != 0 {
 		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "deleting the services in the Kubernetes API")
 
 		namespace := key.ClusterNamespace(customObject)
