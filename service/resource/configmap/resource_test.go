4d16ad729d48e8d322ace45609ad27fb79068b1d
Tim Schindler <xh3b4sd@gmail.com>
2017-09-18 16:42:00 +0200

    added update logic for config map resource

--- service/resource/configmap/resource_test.go
+++ service/resource/configmap/resource_test.go
@@ -2,6 +2,7 @@ package configmap
 
 import (
 	"context"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -662,6 +663,264 @@ func Test_Resource_CloudConfig_GetDeleteState(t *testing.T) {
 			t.Fatalf("case %d expected %d config maps got %d", i+1, len(tc.ExpectedConfigMapNames), len(configMaps))
 		}
 	}
+}
+
+func Test_Resource_CloudConfig_GetUpdateState(t *testing.T) {
+	testCases := []struct {
+		Obj                            interface{}
+		CurrentState                   interface{}
+		DesiredState                   interface{}
+		ExpectedConfigMapNamesToCreate []string
+		ExpectedConfigMapNamesToDelete []string
+		ExpectedConfigMapNamesToUpdate []string
+	}{
+		// Test 1, in case current state and desired state are empty the create,
+		// delete and update state should be empty.
+		{
+			Obj: &kvmtpr.CustomObject{
+				Spec: kvmtpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: clustertprspec.Cluster{
+							ID: "al9qy",
+						},
+					},
+				},
+			},
+			CurrentState:                   []*apiv1.ConfigMap{},
+			DesiredState:                   []*apiv1.ConfigMap{},
+			ExpectedConfigMapNamesToCreate: []string{},
+			ExpectedConfigMapNamesToDelete: []string{},
+			ExpectedConfigMapNamesToUpdate: []string{},
+		},
+
+		// Test 2, in case current state and desired state are equal the create,
+		// delete and update state should be empty.
+		{
+			Obj: &kvmtpr.CustomObject{
+				Spec: kvmtpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: clustertprspec.Cluster{
+							ID: "al9qy",
+						},
+					},
+				},
+			},
+			CurrentState: []*apiv1.ConfigMap{
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-1",
+					},
+					Data: map[string]string{
+						"key1": "val1",
+					},
+				},
+			},
+			DesiredState: []*apiv1.ConfigMap{
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-1",
+					},
+					Data: map[string]string{
+						"key1": "val1",
+					},
+				},
+			},
+			ExpectedConfigMapNamesToCreate: []string{},
+			ExpectedConfigMapNamesToDelete: []string{},
+			ExpectedConfigMapNamesToUpdate: []string{},
+		},
+
+		// Test 3, in case current state misses one item of desired state the delete
+		// state should not contain the missing item of the desired state.
+		{
+			Obj: &kvmtpr.CustomObject{
+				Spec: kvmtpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: clustertprspec.Cluster{
+							ID: "al9qy",
+						},
+					},
+				},
+			},
+			CurrentState: []*apiv1.ConfigMap{},
+			DesiredState: []*apiv1.ConfigMap{
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-1",
+					},
+					Data: map[string]string{
+						"key1": "val1",
+					},
+				},
+			},
+			ExpectedConfigMapNamesToCreate: []string{
+				"config-map-1",
+			},
+			ExpectedConfigMapNamesToDelete: []string{},
+			ExpectedConfigMapNamesToUpdate: []string{},
+		},
+
+		// Test 4, in case current state contains two items and desired state is
+		// missing one of them the delete state should contain the the missing item
+		// from the current state.
+		{
+			Obj: &kvmtpr.CustomObject{
+				Spec: kvmtpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: clustertprspec.Cluster{
+							ID: "al9qy",
+						},
+					},
+				},
+			},
+			CurrentState: []*apiv1.ConfigMap{
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-1",
+					},
+					Data: map[string]string{
+						"key1": "val1",
+					},
+				},
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-2",
+					},
+					Data: map[string]string{
+						"key2": "val2",
+					},
+				},
+			},
+			DesiredState: []*apiv1.ConfigMap{
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-1",
+					},
+					Data: map[string]string{
+						"key1": "val1",
+					},
+				},
+			},
+			ExpectedConfigMapNamesToCreate: []string{},
+			ExpectedConfigMapNamesToDelete: []string{
+				"config-map-2",
+			},
+			ExpectedConfigMapNamesToUpdate: []string{},
+		},
+
+		// Test 5, in case current state contains two items and desired state is
+		// contains the same state but one object is modified internally the update
+		// state should contain the the modified item from the current state.
+		{
+			Obj: &kvmtpr.CustomObject{
+				Spec: kvmtpr.Spec{
+					Cluster: clustertpr.Spec{
+						Cluster: clustertprspec.Cluster{
+							ID: "al9qy",
+						},
+					},
+				},
+			},
+			CurrentState: []*apiv1.ConfigMap{
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-1",
+					},
+					Data: map[string]string{
+						"key1": "val1",
+					},
+				},
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-2",
+					},
+					Data: map[string]string{
+						"key2": "val2-modified",
+					},
+				},
+			},
+			DesiredState: []*apiv1.ConfigMap{
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-1",
+					},
+					Data: map[string]string{
+						"key1": "val1",
+					},
+				},
+				{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Name: "config-map-2",
+					},
+					Data: map[string]string{
+						"key2": "val2",
+					},
+				},
+			},
+			ExpectedConfigMapNamesToCreate: []string{},
+			ExpectedConfigMapNamesToDelete: []string{},
+			ExpectedConfigMapNamesToUpdate: []string{
+				"config-map-2",
+			},
+		},
+	}
+
+	var err error
+	var newResource *Resource
+	{
+		resourceConfig := DefaultConfig()
+		resourceConfig.CertWatcher = certificatetprtest.NewService()
+		resourceConfig.K8sClient = fake.NewSimpleClientset()
+		resourceConfig.Logger = microloggertest.New()
+		newResource, err = New(resourceConfig)
+		if err != nil {
+			t.Fatal("expected", nil, "got", err)
+		}
+	}
+
+	for i, tc := range testCases {
+		createState, deleteState, updateState, err := newResource.GetUpdateState(context.TODO(), tc.Obj, tc.CurrentState, tc.DesiredState)
+		if err != nil {
+			t.Fatalf("case %d expected %#v got %#v", i+1, nil, err)
+		}
+
+		configMapsToCreate, ok := createState.([]*apiv1.ConfigMap)
+		if !ok {
+			t.Fatalf("case %d expected %T got %T", i+1, []*apiv1.ConfigMap{}, createState)
+		}
+		namesToCreate := toConfigMapNames(configMapsToCreate)
+		if !reflect.DeepEqual(namesToCreate, tc.ExpectedConfigMapNamesToCreate) {
+			t.Fatalf("case %d expected %#v got %#v", i+1, tc.ExpectedConfigMapNamesToCreate, namesToCreate)
+		}
+
+		configMapsToDelete, ok := deleteState.([]*apiv1.ConfigMap)
+		if !ok {
+			t.Fatalf("case %d expected %T got %T", i+1, []*apiv1.ConfigMap{}, deleteState)
+		}
+		namesToDelete := toConfigMapNames(configMapsToDelete)
+		if !reflect.DeepEqual(namesToDelete, tc.ExpectedConfigMapNamesToDelete) {
+			t.Fatalf("case %d expected %#v got %#v", i+1, tc.ExpectedConfigMapNamesToDelete, namesToDelete)
+		}
+
+		configMapsToUpdate, ok := updateState.([]*apiv1.ConfigMap)
+		if !ok {
+			t.Fatalf("case %d expected %T got %T", i+1, []*apiv1.ConfigMap{}, updateState)
+		}
+		namesToUpdate := toConfigMapNames(configMapsToUpdate)
+		if !reflect.DeepEqual(namesToUpdate, tc.ExpectedConfigMapNamesToUpdate) {
+			t.Fatalf("case %d expected %#v got %#v", i+1, tc.ExpectedConfigMapNamesToUpdate, namesToUpdate)
+		}
+	}
+}
+
+func toConfigMapNames(configMaps []*apiv1.ConfigMap) []string {
+	names := []string{}
+
+	for _, c := range configMaps {
+		names = append(names, c.Name)
+	}
+
+	return names
 }
 
 func testGetMasterCount(configMaps []*apiv1.ConfigMap) int {
