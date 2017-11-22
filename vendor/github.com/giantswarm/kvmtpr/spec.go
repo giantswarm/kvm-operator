834b1662a09389a8926dd72a61ed1934baf32754
Tim Schindler <xh3b4sd@gmail.com>
2017-11-22 09:21:20 +0100

    updated vendor deps

--- vendor/github.com/giantswarm/kvmtpr/spec.go
+++ vendor/github.com/giantswarm/kvmtpr/spec.go
@@ -2,13 +2,12 @@ package kvmtpr
 
 import (
 	"github.com/giantswarm/clustertpr"
-	"github.com/giantswarm/versionbundle"
 
 	"github.com/giantswarm/kvmtpr/spec"
 )
 
 type Spec struct {
-	Cluster       clustertpr.Spec      `json:"cluster" yaml:"cluster"`
-	KVM           spec.KVM             `json:"kvm" yaml:"kvm"`
-	VersionBundle versionbundle.Bundle `json:"version_bundle" yaml:"version_bundle"`
+	Cluster       clustertpr.Spec    `json:"cluster" yaml:"cluster"`
+	KVM           spec.KVM           `json:"kvm" yaml:"kvm"`
+	VersionBundle spec.VersionBundle `json:"versionBundle" yaml:"versionBundle"`
 }
