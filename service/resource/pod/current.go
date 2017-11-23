523bf941e3b62372bc62673187078db13f387a5b
Tim Schindler <xh3b4sd@gmail.com>
2017-11-23 15:57:34 +0100

    ignore not found errors in GetCurrentState

--- service/resource/pod/current.go
+++ service/resource/pod/current.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/kvm-operator/service/key"
@@ -18,7 +19,14 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interf
 	r.logger.LogCtx(ctx, "debug", "looking for the current version of the reconciled pod in the Kubernetes API")
 
 	currentPod, err := r.k8sClient.CoreV1().Pods(reconciledPod.Namespace).Get(reconciledPod.Name, apismetav1.GetOptions{})
-	if err != nil {
+	if apierrors.IsNotFound(err) {
+		// In case we reconcile a pod we cannot find anymore this means the
+		// informer's watch event is outdated and the pod got already deleted in the
+		// Kubernetes API. This is a normal transition behaviour, so we just ignore
+		// it and assume we are done.
+		r.logger.LogCtx(ctx, "debug", "cannot find the current version of the reconciled pod in the Kubernetes API")
+		return nil, nil
+	} else if err != nil {
 		return nil, microerror.Mask(err)
 	}
 
