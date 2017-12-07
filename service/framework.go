8081a9a0911f750c2da5c09cce32d3b1468180b8
Tim Schindler <xh3b4sd@gmail.com>
2017-12-07 09:28:12 +0100

    ensure CRD (#276)

--- service/framework.go
+++ service/framework.go
@@ -6,35 +6,326 @@ import (
 	"time"
 
 	"github.com/cenkalti/backoff"
+	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/certificatetpr"
 	"github.com/giantswarm/kvmtpr"
 	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger/microloggertest"
 	"github.com/giantswarm/operatorkit/client/k8sclient"
+	"github.com/giantswarm/operatorkit/client/k8scrdclient"
+	"github.com/giantswarm/operatorkit/client/k8sextclient"
 	"github.com/giantswarm/operatorkit/framework"
 	"github.com/giantswarm/operatorkit/framework/resource/metricsresource"
 	"github.com/giantswarm/operatorkit/framework/resource/retryresource"
 	"github.com/giantswarm/operatorkit/informer"
 	"github.com/giantswarm/operatorkit/tpr"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	"github.com/giantswarm/kvm-operator/service/cloudconfigv1"
+	"github.com/giantswarm/kvm-operator/service/cloudconfigv2"
 	"github.com/giantswarm/kvm-operator/service/keyv1"
 	"github.com/giantswarm/kvm-operator/service/messagecontext"
 	"github.com/giantswarm/kvm-operator/service/resource/configmapv1"
+	"github.com/giantswarm/kvm-operator/service/resource/configmapv2"
 	"github.com/giantswarm/kvm-operator/service/resource/deploymentv1"
+	"github.com/giantswarm/kvm-operator/service/resource/deploymentv2"
 	"github.com/giantswarm/kvm-operator/service/resource/ingressv1"
+	"github.com/giantswarm/kvm-operator/service/resource/ingressv2"
 	"github.com/giantswarm/kvm-operator/service/resource/namespacev1"
+	"github.com/giantswarm/kvm-operator/service/resource/namespacev2"
 	"github.com/giantswarm/kvm-operator/service/resource/podv1"
 	"github.com/giantswarm/kvm-operator/service/resource/pvcv1"
+	"github.com/giantswarm/kvm-operator/service/resource/pvcv2"
 	"github.com/giantswarm/kvm-operator/service/resource/servicev1"
+	"github.com/giantswarm/kvm-operator/service/resource/servicev2"
 )
 
 const (
 	ResourceRetries uint64 = 3
 )
+
+func newCRDFramework(config Config) (*framework.Framework, error) {
+	if config.Flag == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Flag must not be empty")
+	}
+	if config.Viper == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Viper must not be empty")
+	}
+
+	var err error
+
+	var k8sExtClient apiextensionsclient.Interface
+	{
+		c := k8sextclient.DefaultConfig()
+
+		c.Logger = config.Logger
+
+		c.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
+		c.InCluster = config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster)
+		c.TLS.CAFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile)
+		c.TLS.CrtFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile)
+		c.TLS.KeyFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile)
+
+		k8sExtClient, err = k8sextclient.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var crdClient *k8scrdclient.CRDClient
+	{
+		c := k8scrdclient.DefaultConfig()
+
+		c.K8sExtClient = k8sExtClient
+		c.Logger = microloggertest.New()
+
+		crdClient, err = k8scrdclient.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var k8sClient kubernetes.Interface
+	{
+		c := k8sclient.DefaultConfig()
+
+		c.Logger = config.Logger
+
+		c.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
+		c.InCluster = config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster)
+		c.TLS.CAFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile)
+		c.TLS.CrtFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile)
+		c.TLS.KeyFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile)
+
+		k8sClient, err = k8sclient.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var certWatcher certificatetpr.Searcher
+	{
+		certConfig := certificatetpr.DefaultServiceConfig()
+
+		certConfig.K8sClient = k8sClient
+		certConfig.Logger = config.Logger
+
+		certWatcher, err = certificatetpr.NewService(certConfig)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var ccService *cloudconfigv2.CloudConfig
+	{
+		c := cloudconfigv2.DefaultConfig()
+
+		c.Logger = config.Logger
+
+		ccService, err = cloudconfigv2.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var configMapResource framework.Resource
+	{
+		c := configmapv2.DefaultConfig()
+
+		c.CertWatcher = certWatcher
+		c.CloudConfig = ccService
+		c.K8sClient = k8sClient
+		c.Logger = config.Logger
+
+		configMapResource, err = configmapv2.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var deploymentResource framework.Resource
+	{
+		c := deploymentv2.DefaultConfig()
+
+		c.K8sClient = k8sClient
+		c.Logger = config.Logger
+
+		deploymentResource, err = deploymentv2.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var ingressResource framework.Resource
+	{
+		c := ingressv2.DefaultConfig()
+
+		c.K8sClient = k8sClient
+		c.Logger = config.Logger
+
+		ingressResource, err = ingressv2.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var namespaceResource framework.Resource
+	{
+		c := namespacev2.DefaultConfig()
+
+		c.K8sClient = k8sClient
+		c.Logger = config.Logger
+
+		namespaceResource, err = namespacev2.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var pvcResource framework.Resource
+	{
+		c := pvcv2.DefaultConfig()
+
+		c.K8sClient = k8sClient
+		c.Logger = config.Logger
+
+		pvcResource, err = pvcv2.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var serviceResource framework.Resource
+	{
+		c := servicev2.DefaultConfig()
+
+		c.K8sClient = k8sClient
+		c.Logger = config.Logger
+
+		serviceResource, err = servicev2.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var resources []framework.Resource
+	{
+		resources = []framework.Resource{
+			namespaceResource,
+
+			configMapResource,
+			deploymentResource,
+			ingressResource,
+			pvcResource,
+			serviceResource,
+		}
+
+		retryWrapConfig := retryresource.DefaultWrapConfig()
+
+		retryWrapConfig.BackOffFactory = func() backoff.BackOff { return backoff.WithMaxTries(backoff.NewExponentialBackOff(), ResourceRetries) }
+		retryWrapConfig.Logger = config.Logger
+
+		resources, err = retryresource.Wrap(resources, retryWrapConfig)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		metricsWrapConfig := metricsresource.DefaultWrapConfig()
+
+		metricsWrapConfig.Name = config.Name
+
+		resources, err = metricsresource.Wrap(resources, metricsWrapConfig)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var clientSet *versioned.Clientset
+	{
+		var c *rest.Config
+
+		if config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster) {
+			config.Logger.Log("debug", "creating in-cluster config")
+
+			c, err = rest.InClusterConfig()
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+		} else {
+			config.Logger.Log("debug", "creating out-cluster config")
+
+			c = &rest.Config{
+				Host: config.Viper.GetString(config.Flag.Service.Kubernetes.Address),
+				TLSClientConfig: rest.TLSClientConfig{
+					CAFile:   config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile),
+					CertFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile),
+					KeyFile:  config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile),
+				},
+			}
+		}
+
+		clientSet, err = versioned.NewForConfig(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var newWatcherFactory informer.WatcherFactory
+	{
+		newWatcherFactory = func() (watch.Interface, error) {
+			watcher, err := clientSet.ProviderV1alpha1().KVMConfigs("").Watch(apismetav1.ListOptions{})
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			return watcher, nil
+		}
+	}
+
+	var newInformer *informer.Informer
+	{
+		c := informer.DefaultConfig()
+
+		c.WatcherFactory = newWatcherFactory
+
+		newInformer, err = informer.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	initCtxFunc := func(ctx context.Context, obj interface{}) (context.Context, error) {
+		message := messagecontext.NewMessage()
+		ctx = messagecontext.NewContext(ctx, message)
+
+		return ctx, nil
+	}
+
+	var crdFramework *framework.Framework
+	{
+		c := framework.DefaultConfig()
+
+		c.CRD = v1alpha1.NewKVMConfigCRD()
+		c.CRDClient = crdClient
+		c.Informer = newInformer
+		c.InitCtxFunc = initCtxFunc
+		c.Logger = config.Logger
+		c.ResourceRouter = framework.DefaultResourceRouter(resources)
+
+		crdFramework, err = framework.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	return crdFramework, nil
+}
 
 func newCustomObjectFramework(config Config) (*framework.Framework, error) {
 	if config.Flag == nil {
@@ -50,8 +341,9 @@ func newCustomObjectFramework(config Config) (*framework.Framework, error) {
 	{
 		c := k8sclient.DefaultConfig()
 
-		c.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
 		c.Logger = config.Logger
+
+		c.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
 		c.InCluster = config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster)
 		c.TLS.CAFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile)
 		c.TLS.CrtFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile)
@@ -281,8 +573,9 @@ func newPodFramework(config Config) (*framework.Framework, error) {
 	{
 		c := k8sclient.DefaultConfig()
 
-		c.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
 		c.Logger = config.Logger
+
+		c.Address = config.Viper.GetString(config.Flag.Service.Kubernetes.Address)
 		c.InCluster = config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster)
 		c.TLS.CAFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile)
 		c.TLS.CrtFile = config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile)
