28a1edb777da39ce7c3351a8b1de04d8e810abf7
Tim Schindler <xh3b4sd@gmail.com>
2017-12-07 15:48:00 +0100

    added first code for TPR migration

--- service/framework.go
+++ service/framework.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -9,9 +10,12 @@ import (
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/certificatetpr"
+	"github.com/giantswarm/clustertpr/spec"
+	"github.com/giantswarm/clustertpr/spec/kubernetes/ssh"
 	"github.com/giantswarm/kvmtpr"
+	"github.com/giantswarm/kvmtpr/spec/kvm"
 	"github.com/giantswarm/microerror"
-	"github.com/giantswarm/micrologger/microloggertest"
+	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/client/k8sclient"
 	"github.com/giantswarm/operatorkit/client/k8scrdclient"
 	"github.com/giantswarm/operatorkit/client/k8sextclient"
@@ -83,7 +87,7 @@ func newCRDFramework(config Config) (*framework.Framework, error) {
 		c := k8scrdclient.DefaultConfig()
 
 		c.K8sExtClient = k8sExtClient
-		c.Logger = microloggertest.New()
+		c.Logger = config.Logger
 
 		crdClient, err = k8scrdclient.New(c)
 		if err != nil {
@@ -275,6 +279,9 @@ func newCRDFramework(config Config) (*framework.Framework, error) {
 			return nil, microerror.Mask(err)
 		}
 	}
+
+	// TODO remove after migration.
+	migrateTPRsToCRDs(config.Logger, clientSet)
 
 	var newWatcherFactory informer.WatcherFactory
 	{
@@ -677,4 +684,181 @@ func newPodFramework(config Config) (*framework.Framework, error) {
 	}
 
 	return podFramework, nil
+}
+
+func migrateTPRsToCRDs(logger micrologger.Logger, clientSet *versioned.Clientset) {
+	logger.Log("debug", "start TPR migration")
+
+	var err error
+
+	// List all TPOs.
+	var b []byte
+	{
+		e := "/apis/cluster.giantswarm.io/v1/namespaces/default/kvms"
+		b, err = clientSet.Discovery().RESTClient().Get().AbsPath(e).DoRaw()
+		if err != nil {
+			logger.Log("error", fmt.Sprintf("%#v", err))
+			return
+		}
+
+		fmt.Printf("\n")
+		fmt.Printf("b start\n")
+		fmt.Printf("%s\n", b)
+		fmt.Printf("b end\n")
+		fmt.Printf("\n")
+	}
+
+	// Convert bytes into structure.
+	var v *kvmtpr.List
+	{
+		if err := json.Unmarshal(b, v); err != nil {
+			logger.Log("error", fmt.Sprintf("%#v", err))
+			return
+		}
+
+		fmt.Printf("\n")
+		fmt.Printf("v start\n")
+		fmt.Printf("%#v\n", v)
+		fmt.Printf("v end\n")
+		fmt.Printf("\n")
+	}
+
+	// Iterate over all TPOs.
+	for _, tpo := range v.Items {
+		// Compute CRO using TPO.
+		var cro *v1alpha1.KVMConfig
+		{
+			cro = &v1alpha1.KVMConfig{}
+
+			cro.Spec.Cluster.Calico.CIDR = tpo.Spec.Cluster.Calico.CIDR
+			cro.Spec.Cluster.Calico.Domain = tpo.Spec.Cluster.Calico.Domain
+			cro.Spec.Cluster.Calico.MTU = tpo.Spec.Cluster.Calico.MTU
+			cro.Spec.Cluster.Calico.Subnet = tpo.Spec.Cluster.Calico.Subnet
+			cro.Spec.Cluster.Customer.ID = tpo.Spec.Cluster.Customer.ID
+			cro.Spec.Cluster.Docker.Daemon.CIDR = tpo.Spec.Cluster.Docker.Daemon.CIDR
+			cro.Spec.Cluster.Docker.Daemon.ExtraArgs = tpo.Spec.Cluster.Docker.Daemon.ExtraArgs
+			cro.Spec.Cluster.Etcd.AltNames = tpo.Spec.Cluster.Etcd.AltNames
+			cro.Spec.Cluster.Etcd.Domain = tpo.Spec.Cluster.Etcd.Domain
+			cro.Spec.Cluster.Etcd.Port = tpo.Spec.Cluster.Etcd.Port
+			cro.Spec.Cluster.Etcd.Prefix = tpo.Spec.Cluster.Etcd.Prefix
+			cro.Spec.Cluster.ID = tpo.Spec.Cluster.Cluster.ID
+			cro.Spec.Cluster.Kubernetes.API.AltNames = tpo.Spec.Cluster.Kubernetes.API.AltNames
+			cro.Spec.Cluster.Kubernetes.API.ClusterIPRange = tpo.Spec.Cluster.Kubernetes.API.ClusterIPRange
+			cro.Spec.Cluster.Kubernetes.API.Domain = tpo.Spec.Cluster.Kubernetes.API.Domain
+			cro.Spec.Cluster.Kubernetes.API.InsecurePort = tpo.Spec.Cluster.Kubernetes.API.InsecurePort
+			cro.Spec.Cluster.Kubernetes.API.IP = tpo.Spec.Cluster.Kubernetes.API.IP
+			cro.Spec.Cluster.Kubernetes.API.SecurePort = tpo.Spec.Cluster.Kubernetes.API.SecurePort
+			cro.Spec.Cluster.Kubernetes.DNS.IP = tpo.Spec.Cluster.Kubernetes.DNS.IP
+			cro.Spec.Cluster.Kubernetes.Domain = tpo.Spec.Cluster.Kubernetes.Domain
+			cro.Spec.Cluster.Kubernetes.Hyperkube.Docker.Image = tpo.Spec.Cluster.Kubernetes.Hyperkube.Docker.Image
+			cro.Spec.Cluster.Kubernetes.IngressController.Docker.Image = tpo.Spec.Cluster.Kubernetes.IngressController.Docker.Image
+			cro.Spec.Cluster.Kubernetes.IngressController.Domain = tpo.Spec.Cluster.Kubernetes.IngressController.Domain
+			cro.Spec.Cluster.Kubernetes.IngressController.InsecurePort = tpo.Spec.Cluster.Kubernetes.IngressController.InsecurePort
+			cro.Spec.Cluster.Kubernetes.IngressController.SecurePort = tpo.Spec.Cluster.Kubernetes.IngressController.SecurePort
+			cro.Spec.Cluster.Kubernetes.IngressController.WildcardDomain = tpo.Spec.Cluster.Kubernetes.IngressController.WildcardDomain
+			cro.Spec.Cluster.Kubernetes.Kubelet.AltNames = tpo.Spec.Cluster.Kubernetes.Kubelet.AltNames
+			cro.Spec.Cluster.Kubernetes.Kubelet.Domain = tpo.Spec.Cluster.Kubernetes.Kubelet.Domain
+			cro.Spec.Cluster.Kubernetes.Kubelet.Labels = tpo.Spec.Cluster.Kubernetes.Kubelet.Labels
+			cro.Spec.Cluster.Kubernetes.Kubelet.Port = tpo.Spec.Cluster.Kubernetes.Kubelet.Port
+			cro.Spec.Cluster.Kubernetes.NetworkSetup.Docker.Image = tpo.Spec.Cluster.Kubernetes.NetworkSetup.Docker.Image
+			cro.Spec.Cluster.Kubernetes.SSH.UserList = toUserList(tpo.Spec.Cluster.Kubernetes.SSH.UserList)
+			cro.Spec.Cluster.Masters = toClusterMasters(tpo.Spec.Cluster.Masters)
+			cro.Spec.Cluster.Vault.Address = tpo.Spec.Cluster.Vault.Address
+			cro.Spec.Cluster.Vault.Token = tpo.Spec.Cluster.Vault.Token
+			cro.Spec.Cluster.Workers = toClusterWorkers(tpo.Spec.Cluster.Workers)
+			cro.Spec.KVM.EndpointUpdater.Docker.Image = tpo.Spec.KVM.EndpointUpdater.Docker.Image
+			cro.Spec.KVM.K8sKVM.Docker.Image = tpo.Spec.KVM.K8sKVM.Docker.Image
+			cro.Spec.KVM.K8sKVM.StorageType = tpo.Spec.KVM.K8sKVM.StorageType
+			cro.Spec.KVM.Masters = toKVMMasters(tpo.Spec.KVM.Masters)
+			cro.Spec.KVM.Network.Flannel.VNI = tpo.Spec.KVM.Network.Flannel.VNI
+			cro.Spec.KVM.NodeController.Docker.Image = tpo.Spec.KVM.NodeController.Docker.Image
+			cro.Spec.KVM.Workers = toKVMWorkers(tpo.Spec.KVM.Workers)
+			cro.Spec.VersionBundle.Version = tpo.Spec.VersionBundle.Version
+
+			fmt.Printf("\n")
+			fmt.Printf("cro start\n")
+			fmt.Printf("%#v\n", cro)
+			fmt.Printf("cro end\n")
+			fmt.Printf("\n")
+		}
+
+		// TODO create CRO in Kubernetes API.
+	}
+
+	logger.Log("debug", "end TPR migration")
+}
+
+func toClusterMasters(masters []spec.Node) []v1alpha1.ClusterNode {
+	var newList []v1alpha1.ClusterNode
+
+	for _, master := range masters {
+		n := v1alpha1.ClusterNode{
+			ID: master.ID,
+		}
+
+		newList = append(newList, n)
+	}
+
+	return newList
+}
+
+func toClusterWorkers(workers []spec.Node) []v1alpha1.ClusterNode {
+	var newList []v1alpha1.ClusterNode
+
+	for _, worker := range workers {
+		n := v1alpha1.ClusterNode{
+			ID: worker.ID,
+		}
+
+		newList = append(newList, n)
+	}
+
+	return newList
+}
+
+func toKVMMasters(masters []kvm.Node) []v1alpha1.KVMConfigSpecKVMNode {
+	var newList []v1alpha1.KVMConfigSpecKVMNode
+
+	for _, master := range masters {
+		w := v1alpha1.KVMConfigSpecKVMNode{
+			CPUs:   master.CPUs,
+			Disk:   master.Disk,
+			Memory: master.Memory,
+		}
+
+		newList = append(newList, w)
+	}
+
+	return newList
+}
+
+func toKVMWorkers(workers []kvm.Node) []v1alpha1.KVMConfigSpecKVMNode {
+	var newList []v1alpha1.KVMConfigSpecKVMNode
+
+	for _, worker := range workers {
+		w := v1alpha1.KVMConfigSpecKVMNode{
+			CPUs:   worker.CPUs,
+			Disk:   worker.Disk,
+			Memory: worker.Memory,
+		}
+
+		newList = append(newList, w)
+	}
+
+	return newList
+}
+
+func toUserList(userList []ssh.User) []v1alpha1.ClusterKubernetesSSHUser {
+	var newList []v1alpha1.ClusterKubernetesSSHUser
+
+	for _, user := range userList {
+		u := v1alpha1.ClusterKubernetesSSHUser{
+			Name:      user.Name,
+			PublicKey: user.PublicKey,
+		}
+
+		newList = append(newList, u)
+	}
+
+	return newList
 }
