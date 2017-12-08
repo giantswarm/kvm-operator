210cbd2c1ab68709133cc3742a7743c35fd6e5e6
Julien Garcia Gonzalez <julien@giantswarm.io>
2017-12-08 11:49:44 +0100

    fix typo
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- service/cloudconfigv1/template.go
+++ service/cloudconfigv1/template.go
@@ -6,6 +6,6 @@ Before=set-ownership-etcd-data-dir.service
 `
 	set_ownership_etcd_data_dir_dropin = `[Unit]
 Requires=etc-kubernetes-data-etcd.mount
-After=etc_kubernetes_data_etcd.mount
+After=etc-kubernetes-data-etcd.mount
 `
 )
