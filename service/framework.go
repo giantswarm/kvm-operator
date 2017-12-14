f1611ba229d34d832855613e76fa4384428d0582
Fernando Ripoll <pipo02mix@gmail.com>
2017-12-14 11:58:51 +0100

    make signature for resource router looks equal as AWS

--- service/framework.go
+++ service/framework.go
@@ -229,6 +229,13 @@ func newCRDFramework(config Config) (*framework.Framework, error) {
 		}
 	}
 
+	// We provide a map of resource lists keyed by the version bundle version
+	// to the resource router.
+	versionedResources := map[string][]framework.Resource{
+		"1.0.0": resources,
+		"0.1.0": resources,
+	}
+
 	var newInformer *informer.Informer
 	{
 		c := informer.DefaultConfig()
@@ -257,7 +264,7 @@ func newCRDFramework(config Config) (*framework.Framework, error) {
 		c.Informer = newInformer
 		c.InitCtxFunc = initCtxFunc
 		c.Logger = config.Logger
-		c.ResourceRouter = newResourceRouter(resources)
+		c.ResourceRouter = newResourceRouter(versionedResources)
 
 		crdFramework, err = framework.New(c)
 		if err != nil {
--- service/resource_router.go
+++ service/resource_router.go
@@ -10,22 +10,22 @@ import (
 
 // newResourceRouter determines which resources are enabled based upon the
 // version in the version bundle.
-func newResourceRouter(resources []framework.Resource) func(ctx context.Context, obj interface{}) ([]framework.Resource, error) {
+func newResourceRouter(resources map[string][]framework.Resource) func(ctx context.Context, obj interface{}) ([]framework.Resource, error) {
 	return func(ctx context.Context, obj interface{}) ([]framework.Resource, error) {
 		var enabledResources []framework.Resource
 
 		customObject, err := keyv2.ToCustomObject(obj)
 		if err != nil {
-			return enabledResources, microerror.Mask(err)
+			return nil, microerror.Mask(err)
 		}
 
 		switch keyv2.VersionBundleVersion(customObject) {
 		case "0.1.0":
-			enabledResources = resources
+			enabledResources = resources["0.1.0"]
 		case "1.0.0":
-			enabledResources = resources
+			enabledResources = resources["1.0.0"]
 		default:
-			return enabledResources, microerror.Maskf(invalidVersionError, "version '%s' in version bundle is invalid", keyv2.VersionBundleVersion(customObject))
+			return nil, microerror.Maskf(invalidVersionError, "version '%s' in version bundle is invalid", keyv2.VersionBundleVersion(customObject))
 		}
 
 		return enabledResources, nil
