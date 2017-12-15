c1632279d8125b32956d3eb547f219402dadb0ac
Julien Garcia Gonzalez <garciagonzalez.julien@gmail.com>
2017-12-15 17:05:11 +0100

    Adding configmap and cloudconfig v3 (#318)
    * copy cloudconfigv2 to v3
    
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>
    
    * copy configmapv2 to v3
    
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

---
+++ service/cloudconfigv3/master_template.go
@@ -0,0 +1,227 @@
+package cloudconfigv2
+
+import (
+	"github.com/giantswarm/certs"
+	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_1_1_0"
+	"github.com/giantswarm/microerror"
+)
+
+type masterExtension struct {
+	certs certs.Cluster
+}
+
+func (e *masterExtension) Files() ([]k8scloudconfig.FileAsset, error) {
+	filesMeta := []k8scloudconfig.FileMetadata{
+		// Kubernetes API server.
+		{
+			AssetContent: string(e.certs.APIServer.CA),
+			Path:         "/etc/kubernetes/ssl/apiserver-ca.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.APIServer.Crt),
+			Path:         "/etc/kubernetes/ssl/apiserver-crt.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.APIServer.Key),
+			Path:         "/etc/kubernetes/ssl/apiserver-key.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		// Calico client.
+		{
+			AssetContent: string(e.certs.CalicoClient.CA),
+			Path:         "/etc/kubernetes/ssl/calico/client-ca.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.CalicoClient.Crt),
+			Path:         "/etc/kubernetes/ssl/calico/client-crt.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.CalicoClient.Key),
+			Path:         "/etc/kubernetes/ssl/calico/client-key.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		// Etcd client.
+		{
+			AssetContent: string(e.certs.EtcdServer.CA),
+			Path:         "/etc/kubernetes/ssl/etcd/client-ca.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.EtcdServer.Crt),
+			Path:         "/etc/kubernetes/ssl/etcd/client-crt.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.EtcdServer.Key),
+			Path:         "/etc/kubernetes/ssl/etcd/client-key.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		// Etcd server.
+		{
+			AssetContent: string(e.certs.EtcdServer.CA),
+			Path:         "/etc/kubernetes/ssl/etcd/server-ca.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.EtcdServer.Crt),
+			Path:         "/etc/kubernetes/ssl/etcd/server-crt.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.EtcdServer.Key),
+			Path:         "/etc/kubernetes/ssl/etcd/server-key.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		// Service account.
+		{
+			AssetContent: string(e.certs.ServiceAccount.CA),
+			Path:         "/etc/kubernetes/ssl/service-account-ca.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.ServiceAccount.Crt),
+			Path:         "/etc/kubernetes/ssl/service-account-crt.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		{
+			AssetContent: string(e.certs.ServiceAccount.Key),
+			Path:         "/etc/kubernetes/ssl/service-account-key.pem",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+		// set_ownership_etcd_data_dir drop-in
+		{
+			AssetContent: set_ownership_etcd_data_dir_dropin,
+			Path:         "/etc/systemd/system/set-ownership-etcd-data-dir.service.d/00-after-mount.conf",
+			Owner:        FileOwner,
+			Permissions:  FilePermission,
+		},
+	}
+
+	var newFiles []k8scloudconfig.FileAsset
+
+	for _, fm := range filesMeta {
+		c, err := k8scloudconfig.RenderAssetContent(fm.AssetContent, nil)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		fileAsset := k8scloudconfig.FileAsset{
+			Metadata: fm,
+			Content:  c,
+		}
+
+		newFiles = append(newFiles, fileAsset)
+	}
+
+	return newFiles, nil
+}
+
+func (e *masterExtension) Units() ([]k8scloudconfig.UnitAsset, error) {
+	unitsMeta := []k8scloudconfig.UnitMetadata{
+		// Mount etcd volume when directory first accessed
+		// This automount is workaround for
+		// https://bugzilla.redhat.com/show_bug.cgi?id=1184122
+		{
+			AssetContent: `[Unit]
+Description=Automount for etcd volume
+[Automount]
+Where=/etc/kubernetes/data/etcd
+[Install]
+WantedBy=multi-user.target
+`,
+			Name:    "etc-kubernetes-data-etcd.automount",
+			Enable:  true,
+			Command: "start",
+		},
+		// Mount for etcd volume activated by automount
+		{
+			AssetContent: `[Unit]
+Description=Mount for etcd volume
+[Mount]
+What=etcdshare
+Where=/etc/kubernetes/data/etcd
+Options=trans=virtio,version=9p2000.L,cache=mmap
+Type=9p
+[Install]
+WantedBy=multi-user.target
+`,
+			Name:   "etc-kubernetes-data-etcd.mount",
+			Enable: false,
+		},
+		{
+			Name:    "iscsid.service",
+			Enable:  true,
+			Command: "start",
+		},
+		{
+			Name:    "multipathd.service",
+			Enable:  true,
+			Command: "start",
+		},
+		{
+			AssetContent: `[Unit]
+Description=Temporary fix for issues with calico-node and kube-proxy after master restart
+Require=k8s-kubelet.service
+After=k8s-kubelet.service
+
+[Service]
+Environment="KUBECONFIG=/etc/kubernetes/config/addons-kubeconfig.yml"
+Environment="KUBECTL=quay.io/giantswarm/docker-kubectl:1dc536ec6dc4597ba46769b3d5d6ce53a7e62038"
+ExecStart=/bin/sh -c "\
+	while [ \"$(/usr/bin/docker run -e KUBECONFIG=${KUBECONFIG} --net=host --rm -v /etc/kubernetes:/etc/kubernetes $KUBECTL get cs | grep Healthy | wc -l)\" -ne \"3\" ]; do sleep 1 && echo 'Waiting for healthy k8s'; done;sleep 30s; \
+	/usr/bin/docker run -e KUBECONFIG=${KUBECONFIG} --net=host --rm -v /etc/kubernetes:/etc/kubernetes $KUBECTL -n kube-system delete pod -l k8s-app=calico-node; \
+	sleep 1m; \
+	/usr/bin/docker run -e KUBECONFIG=${KUBECONFIG} --net=host --rm -v /etc/kubernetes:/etc/kubernetes $KUBECTL -n kube-system delete pod -l k8s-app=kube-proxy; \
+	sleep 1m; \
+	/usr/bin/docker run -e KUBECONFIG=${KUBECONFIG} --net=host --rm -v /etc/kubernetes:/etc/kubernetes $KUBECTL -n kube-system delete pod -l k8s-app=calico-kube-controllers; \
+	/usr/bin/docker run -e KUBECONFIG=${KUBECONFIG} --net=host --rm -v /etc/kubernetes:/etc/kubernetes $KUBECTL -n kube-system delete pod -l k8s-app=kube-dns"
+
+[Install]
+WantedBy=multi-user.target`,
+			Name:    "calico-kube-kill.service",
+			Command: "start",
+			Enable:  true,
+		},
+	}
+
+	var newUnits []k8scloudconfig.UnitAsset
+
+	for _, fm := range unitsMeta {
+		c, err := k8scloudconfig.RenderAssetContent(fm.AssetContent, nil)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		unitAsset := k8scloudconfig.UnitAsset{
+			Metadata: fm,
+			Content:  c,
+		}
+
+		newUnits = append(newUnits, unitAsset)
+	}
+
+	return newUnits, nil
+}
+
+func (e *masterExtension) VerbatimSections() []k8scloudconfig.VerbatimSection {
+	return nil
+}
