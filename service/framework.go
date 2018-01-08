cb373aa03d4102c3a31d6da34f26c257b7db9c2c
Tim Schindler <xh3b4sd@gmail.com>
2018-01-08 11:52:15 +0100

    set update allowed to context when operator is configured accordingly (#338)

--- service/framework.go
+++ service/framework.go
@@ -25,6 +25,7 @@ import (
 	"github.com/giantswarm/kvm-operator/service/cloudconfigv2"
 	"github.com/giantswarm/kvm-operator/service/cloudconfigv3"
 	"github.com/giantswarm/kvm-operator/service/keyv2"
+	"github.com/giantswarm/kvm-operator/service/keyv3"
 	"github.com/giantswarm/kvm-operator/service/resource/configmapv2"
 	"github.com/giantswarm/kvm-operator/service/resource/configmapv3"
 	"github.com/giantswarm/kvm-operator/service/resource/deploymentv2"
@@ -337,8 +338,18 @@ func newCRDFramework(config Config) (*framework.Framework, error) {
 	}
 
 	initCtxFunc := func(ctx context.Context, obj interface{}) (context.Context, error) {
-		if config.Viper.GetBool(config.Flag.Service.Guest.Update.Enabled) {
-			updateallowedcontext.SetUpdateAllowed(ctx)
+		{
+			customObject, err := keyv3.ToCustomObject(obj)
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			updateEnabled := config.Viper.GetBool(config.Flag.Service.Guest.Update.Enabled)
+			versionBundleVersion := keyv3.VersionBundleVersion(customObject)
+
+			if updateEnabled && versionBundleVersion >= "1.1.0" {
+				updateallowedcontext.SetUpdateAllowed(ctx)
+			}
 		}
 
 		return ctx, nil
