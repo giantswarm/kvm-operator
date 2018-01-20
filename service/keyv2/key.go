0a2c4dad033c6d0c9bf18c35b831aa80bf929d0c
Vaclav Rozsypalek <vaclav@giantswarm.io>
2018-01-20 13:57:27 +0100

    final docker image update

--- service/keyv2/key.go
+++ service/keyv2/key.go
@@ -37,7 +37,7 @@ const (
 	FlannelEnvPathPrefix = "/run/flannel"
 
 	K8SKVMHealthDocker       = "quay.io/giantswarm/k8s-kvm-health:ddf211dfed52086ade32ab8c45e44eb0273319ef"
-	K8SEndpointUpdaterDocker = "quay.io/giantswarm/k8s-endpoint-updater:90d56113aa6ea53a9f233cbfdb8f43c21be6bfbe"
+	K8SEndpointUpdaterDocker = "quay.io/giantswarm/k8s-endpoint-updater:df982fc73b71e60fc70a7444c068b52441ddb30e"
 	// kvm endpoint annotations
 	AnnotationIp      = "endpoint.kvm.giantswarm.io/ip"
 	AnnotationService = "endpoint.kvm.giantswarm.io/service"
