0e8ed6d629a277e2c86625d746a6b23528eb4756
Fernando Ripoll <pipo02mix@gmail.com>
2018-01-22 17:19:48 +0100

    Leaving extra line to avoid gofmt error

--- service/resource/serviceaccountv3/current.go
+++ service/resource/serviceaccountv3/current.go
@@ -6,6 +6,7 @@ import (
 	"github.com/giantswarm/microerror"
 	apiv1 "k8s.io/api/core/v1"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/giantswarm/kvm-operator/service/keyv3"
