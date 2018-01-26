2e2a0512e85b2d7784810b8403b23472da3c1646
Paweł Kopiczko <kopiczko@gmail.com>
2018-01-26 16:17:23 +0100

    fix import

--- service/pod/resource/podv2/current.go
+++ service/pod/resource/podv2/current.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/giantswarm/microerror"
 
-	"github.com/giantswarm/kvm-operator/service/keyv2"
+	"github.com/giantswarm/kvm-operator/service/pod/keyv2"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
