1fb106d32dae1df5c31808dbc8d35469f91ab048
calvix <calvin_24@seznam.cz>
2018-02-20 16:28:43 +0100

    update-kvm-node-controller (#399)
    * update-kvm-node-controller
    
    * smalfix

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -292,5 +292,50 @@ func NewVersionBundles() []versionbundle.Bundle {
 			Version:      "2.0.0",
 			WIP:          false,
 		},
+		{
+			Changelogs: []versionbundle.Changelog{
+				{
+					Component:   "kvm-node-controller",
+					Description: "Updated KVM node controller with pod status bugfix.",
+					Kind:        versionbundle.KindChanged,
+				},
+			},
+			Components: []versionbundle.Component{
+				{
+					Name:    "calico",
+					Version: "3.0.1",
+				},
+				{
+					Name:    "containerlinux",
+					Version: "1576.5.0",
+				},
+				{
+					Name:    "docker",
+					Version: "17.09.0",
+				},
+				{
+					Name:    "etcd",
+					Version: "3.2.7",
+				},
+				{
+					Name:    "coredns",
+					Version: "1.0.5",
+				},
+				{
+					Name:    "kubernetes",
+					Version: "1.9.2",
+				},
+				{
+					Name:    "nginx-ingress-controller",
+					Version: "0.10.2",
+				},
+			},
+			Dependencies: []versionbundle.Dependency{},
+			Deprecated:   false,
+			Name:         "kvm-operator",
+			Time:         time.Date(2018, time.February, 20, 2, 57, 0, 0, time.UTC),
+			Version:      "2.0.1",
+			WIP:          true,
+		},
 	}
 }
