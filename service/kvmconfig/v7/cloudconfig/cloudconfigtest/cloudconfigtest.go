93f516ce5fb122e4b2a6d9cec55f409039cb237d
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-03-01 17:54:11 +0200

    kvmconfig/v7: copy v6 (#417)

---
+++ service/kvmconfig/v7/cloudconfig/cloudconfigtest/cloudconfigtest.go
@@ -0,0 +1,19 @@
+package cloudconfigtest
+
+import (
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/cloudconfig"
+	"github.com/giantswarm/micrologger/microloggertest"
+)
+
+func New() *cloudconfig.CloudConfig {
+	c := cloudconfig.DefaultConfig()
+
+	c.Logger = microloggertest.New()
+
+	newCloudConfig, err := cloudconfig.New(c)
+	if err != nil {
+		panic(err)
+	}
+
+	return newCloudConfig
+}
