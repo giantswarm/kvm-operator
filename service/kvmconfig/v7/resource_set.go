dbcdebbe74f4f187fd357b98bac35162202f01d7
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-03-01 17:47:07 +0200

    kvmconfig/v7: copy v6

---
+++ service/kvmconfig/v7/resource_set.go
@@ -0,0 +1,253 @@
+package v6
+
+import (
+	"context"
+
+	"github.com/cenkalti/backoff"
+	"github.com/giantswarm/certs"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/framework"
+	"github.com/giantswarm/operatorkit/framework/context/updateallowedcontext"
+	"github.com/giantswarm/operatorkit/framework/resource/metricsresource"
+	"github.com/giantswarm/operatorkit/framework/resource/retryresource"
+	"github.com/giantswarm/randomkeys"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/cloudconfig"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/key"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/clusterrolebinding"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/configmap"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/deployment"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/ingress"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/namespace"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/pvc"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/service"
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v6/resource/serviceaccount"
+)
+
+const (
+	ResourceRetries uint64 = 3
+)
+
+type ResourceSetConfig struct {
+	CertsSearcher      certs.Interface
+	K8sClient          kubernetes.Interface
+	Logger             micrologger.Logger
+	RandomkeysSearcher randomkeys.Interface
+
+	GuestUpdateEnabled bool
+	ProjectName        string
+}
+
+func NewResourceSet(config ResourceSetConfig) (*framework.ResourceSet, error) {
+	var err error
+
+	if config.CertsSearcher == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.CertsSearcher must not be empty")
+	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.K8sClient must not be empty")
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+	if config.RandomkeysSearcher == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.RandomkeysSearcher must not be empty")
+	}
+
+	if config.ProjectName == "" {
+		return nil, microerror.Maskf(invalidConfigError, "config.ProjectName must not be empty")
+	}
+
+	var cloudConfig *cloudconfig.CloudConfig
+	{
+		c := cloudconfig.DefaultConfig()
+
+		c.Logger = config.Logger
+
+		cloudConfig, err = cloudconfig.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var clusterRoleBindingResource framework.Resource
+	{
+		c := clusterrolebinding.Config{
+			K8sClient: config.K8sClient,
+			Logger:    config.Logger,
+		}
+
+		clusterRoleBindingResource, err = clusterrolebinding.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var namespaceResource framework.Resource
+	{
+		c := namespace.DefaultConfig()
+
+		c.K8sClient = config.K8sClient
+		c.Logger = config.Logger
+
+		namespaceResource, err = namespace.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var serviceAccountResource framework.Resource
+	{
+		c := serviceaccount.DefaultConfig()
+
+		c.K8sClient = config.K8sClient
+		c.Logger = config.Logger
+
+		serviceAccountResource, err = serviceaccount.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var configMapResource framework.Resource
+	{
+		c := configmap.DefaultConfig()
+
+		c.CertSearcher = config.CertsSearcher
+		c.CloudConfig = cloudConfig
+		c.K8sClient = config.K8sClient
+		c.KeyWatcher = config.RandomkeysSearcher
+		c.Logger = config.Logger
+
+		configMapResource, err = configmap.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var deploymentResource framework.Resource
+	{
+		c := deployment.DefaultConfig()
+
+		c.K8sClient = config.K8sClient
+		c.Logger = config.Logger
+
+		deploymentResource, err = deployment.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var ingressResource framework.Resource
+	{
+		c := ingress.DefaultConfig()
+
+		c.K8sClient = config.K8sClient
+		c.Logger = config.Logger
+
+		ingressResource, err = ingress.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var pvcResource framework.Resource
+	{
+		c := pvc.DefaultConfig()
+
+		c.K8sClient = config.K8sClient
+		c.Logger = config.Logger
+
+		pvcResource, err = pvc.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var serviceResource framework.Resource
+	{
+		c := service.DefaultConfig()
+
+		c.K8sClient = config.K8sClient
+		c.Logger = config.Logger
+
+		serviceResource, err = service.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	resources := []framework.Resource{
+		clusterRoleBindingResource,
+		namespaceResource,
+		serviceAccountResource,
+		configMapResource,
+		deploymentResource,
+		ingressResource,
+		pvcResource,
+		serviceResource,
+	}
+
+	{
+		c := retryresource.WrapConfig{
+			BackOffFactory: func() backoff.BackOff { return backoff.WithMaxTries(backoff.NewExponentialBackOff(), ResourceRetries) },
+			Logger:         config.Logger,
+		}
+
+		resources, err = retryresource.Wrap(resources, c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	{
+		c := metricsresource.WrapConfig{
+			Name: config.ProjectName,
+		}
+
+		resources, err = metricsresource.Wrap(resources, c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	handlesFunc := func(obj interface{}) bool {
+		kvmConfig, err := key.ToCustomObject(obj)
+		if err != nil {
+			return false
+		}
+
+		if key.VersionBundleVersion(kvmConfig) == VersionBundle().Version {
+			return true
+		}
+
+		return false
+	}
+
+	initCtxFunc := func(ctx context.Context, obj interface{}) (context.Context, error) {
+		if config.GuestUpdateEnabled {
+			updateallowedcontext.SetUpdateAllowed(ctx)
+		}
+
+		return ctx, nil
+	}
+
+	var resourceSet *framework.ResourceSet
+	{
+		c := framework.ResourceSetConfig{
+			Handles:   handlesFunc,
+			InitCtx:   initCtxFunc,
+			Logger:    config.Logger,
+			Resources: resources,
+		}
+
+		resourceSet, err = framework.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	return resourceSet, nil
+}
---
+++ service/kvmconfig/v7/version_bundle.go
@@ -0,0 +1,80 @@
+package v6
+
+import (
+	"time"
+
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "kvm-node-controller",
+				Description: "Updated KVM node controller with pod status bugfix.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "Calico",
+				Description: "Updated to 3.0.2.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "kubelet",
+				Description: "Tune kubelet flags for protecting key units (kubelet and container runtime) from workload overloads.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "etcd",
+				Description: "Updated to 3.3.1.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "qemu",
+				Description: "Fixed formula for calculating qemu memory overhead.",
+				Kind:        versionbundle.KindFixed,
+			},
+			{
+				Component:   "monitoring",
+				Description: "Added configuration for monitoring endpoint IP addresses.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.0.2",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "1576.5.0",
+			},
+			{
+				Name:    "docker",
+				Version: "17.09.0",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.1",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.0.5",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.9.2",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.10.2",
+			},
+		},
+		Dependencies: []versionbundle.Dependency{},
+		Deprecated:   false,
+		Name:         "kvm-operator",
+		Time:         time.Date(2018, time.February, 20, 2, 57, 0, 0, time.UTC),
+		Version:      "2.0.1",
+		WIP:          true,
+	}
+}
