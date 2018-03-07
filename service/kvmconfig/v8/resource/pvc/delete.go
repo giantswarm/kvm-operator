cc7dc1daf4811877d2d2b2f607831177795d9bdc
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-03-07 16:39:03 +0100

    kvmconfig/v8: copy v7 (#425)

---
+++ service/kvmconfig/v8/resource/pvc/delete.go
@@ -0,0 +1,82 @@
+package pvc
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/framework"
+	apiv1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/kvm-operator/service/kvmconfig/v7/key"
+)
+
+func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	pvcsToDelete, err := toPVCs(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if len(pvcsToDelete) != 0 {
+		r.logger.LogCtx(ctx, "debug", "deleting the PVCs in the Kubernetes API")
+
+		namespace := key.ClusterNamespace(customObject)
+		for _, PVC := range pvcsToDelete {
+			err := r.k8sClient.Core().PersistentVolumeClaims(namespace).Delete(PVC.Name, &apismetav1.DeleteOptions{})
+			if apierrors.IsNotFound(err) {
+				// fall through
+			} else if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+
+		r.logger.LogCtx(ctx, "debug", "deleted the PVCs in the Kubernetes API")
+	} else {
+		r.logger.LogCtx(ctx, "debug", "the PVCs do not need to be deleted from the Kubernetes API")
+	}
+
+	return nil
+}
+
+func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
+	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := framework.NewPatch()
+	patch.SetDeleteChange(delete)
+
+	return patch, nil
+}
+
+func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentPVCs, err := toPVCs(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredPVCs, err := toPVCs(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "debug", "finding out which PVCs have to be deleted")
+
+	var pvcsToDelete []*apiv1.PersistentVolumeClaim
+
+	for _, currentPVC := range currentPVCs {
+		if containsPVC(desiredPVCs, currentPVC) {
+			pvcsToDelete = append(pvcsToDelete, currentPVC)
+		}
+	}
+
+	r.logger.LogCtx(ctx, "debug", fmt.Sprintf("found %d PVCs that have to be deleted", len(pvcsToDelete)))
+
+	return pvcsToDelete, nil
+}
