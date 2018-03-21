4bcc30f8082a6d49353a53d9627fa8fe76c167e1
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-03-21 21:29:17 +0200

    Switch to cloudconfig 3.2.3

--- service/kvmconfig/v9/cloudconfig/master_template.go
+++ service/kvmconfig/v9/cloudconfig/master_template.go
@@ -2,7 +2,7 @@ package cloudconfig
 
 import (
 	"github.com/giantswarm/certs"
-	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_3_2_2"
+	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_3_2_3"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/randomkeys"
 )
