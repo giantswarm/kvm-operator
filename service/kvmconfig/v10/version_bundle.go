5ecd9e4fc1d6081af06265bad8724e682d88d7d3
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-04-04 16:25:23 +0300

    Release kvm-operator 2.1.3

--- service/kvmconfig/v10/version_bundle.go
+++ service/kvmconfig/v10/version_bundle.go
@@ -55,6 +55,6 @@ func VersionBundle() versionbundle.Bundle {
 		Name:         "kvm-operator",
 		Time:         time.Date(2018, time.March, 22, 15, 30, 0, 0, time.UTC),
 		Version:      "2.1.3",
-		WIP:          true,
+		WIP:          false,
 	}
 }
