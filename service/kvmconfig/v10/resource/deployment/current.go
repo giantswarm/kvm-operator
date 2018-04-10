6c393cfaf3132c7ff041b2017094db4610ced197
Tim Schindler <xh3b4sd@gmail.com>
2018-04-10 12:16:38 +0200

    moves metric package on service level

--- service/kvmconfig/v10/resource/deployment/current.go
+++ service/kvmconfig/v10/resource/deployment/current.go
@@ -11,7 +11,7 @@ import (
 	"k8s.io/api/extensions/v1beta1"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/giantswarm/kvm-operator/service/kvmconfig/metric"
+	"github.com/giantswarm/kvm-operator/service/metric"
 	"github.com/giantswarm/kvm-operator/service/kvmconfig/v10/key"
 )
 
