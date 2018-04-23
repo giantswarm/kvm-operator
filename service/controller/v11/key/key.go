600b04bdf11793c9f43f13facd34b30fd39da488
Tim Schindler <xh3b4sd@gmail.com>
2018-04-23 09:58:32 +0200

    updaes K8sKVM Docker image

--- service/controller/v11/key/key.go
+++ service/controller/v11/key/key.go
@@ -40,7 +40,7 @@ const (
 	CoreosVersion        = "1688.5.3"
 
 	K8SEndpointUpdaterDocker  = "quay.io/giantswarm/k8s-endpoint-updater:df982fc73b71e60fc70a7444c068b52441ddb30e"
-	K8SKVMDockerImage         = "quay.io/giantswarm/k8s-kvm:4438d70d2181af66ea2b90c7f3bc74d1aa3c55a1"
+	K8SKVMDockerImage         = "quay.io/giantswarm/k8s-kvm:2d869988b9de7f9b8b4f36af7abda85dc23d5453"
 	K8SKVMHealthDocker        = "quay.io/giantswarm/k8s-kvm-health:ddf211dfed52086ade32ab8c45e44eb0273319ef"
 	NodeControllerDockerImage = "quay.io/giantswarm/kvm-operator-node-controller:7146561e54142d4f986daee0206336ebee3ceb18"
 
