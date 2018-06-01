ac2a6ef33bf568b78ace647d1d5cd5cceb7f695b
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-06-01 13:32:44 +0300

    controller/v13: copy v12

---
+++ service/controller/v13/resource/deployment/desired.go
@@ -0,0 +1,46 @@
+package deployment
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"k8s.io/api/extensions/v1beta1"
+
+	"github.com/giantswarm/kvm-operator/service/controller/v12/key"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "computing the new deployments")
+
+	var deployments []*v1beta1.Deployment
+
+	{
+		masterDeployments, err := newMasterDeployments(customObject)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+		deployments = append(deployments, masterDeployments...)
+
+		workerDeployments, err := newWorkerDeployments(customObject)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+		deployments = append(deployments, workerDeployments...)
+
+		nodeControllerDeployment, err := newNodeControllerDeployment(customObject)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+		deployments = append(deployments, nodeControllerDeployment)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("computed the %d new deployments", len(deployments)))
+
+	return deployments, nil
+}
