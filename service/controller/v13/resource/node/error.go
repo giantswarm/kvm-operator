71d8c1981b83d365007e882da2d6a68f206fc514
Tim Schindler <xh3b4sd@gmail.com>
2018-06-07 19:01:03 +0200

    extends error handling

--- service/controller/v13/resource/node/error.go
+++ service/controller/v13/resource/node/error.go
@@ -15,28 +15,35 @@ func IsInvalidConfig(err error) bool {
 
 const (
 	// guestDNSNotReadyPattern is a regular expression representing DNS errors for
-	// the guest API domain.
-	// match example https://play.golang.org/p/ipBkwqlc4Td
+	// the guest API domain. Also see the following match example.
+	//
+	//     https://play.golang.org/p/ipBkwqlc4Td
+	//
 	guestDNSNotReadyPattern = "dial tcp: lookup .* on .*:53: no such host"
+
+	// guestEOFPattern is a regular expression representing EOF errors for the
+	// guest API domain. Also see the following match example.
+	//
+	//     https://play.golang.org/p/L6f4ItJLufv
+	//
+	guestEOFPattern = `Get https://api\..*/api/v1/nodes: EOF`
 
 	// guestTransientInvalidCertificatePattern regular expression defines the kind
 	// of transient errors related to certificates returned while the guest API is
-	// not fully up.
-	// match example https://play.golang.org/p/iiYvBhPOg4f
+	// not fully up. Also see the following match example.
+	//
+	//     https://play.golang.org/p/iiYvBhPOg4f
+	//
 	guestTransientInvalidCertificatePattern = `[Get|Post] https://api\..*: x509: certificate is valid for ingress.local, not api\..*`
 )
 
 var (
-	guestDNSNotReadyRegexp                 *regexp.Regexp
-	guestTransientInvalidCertificateRegexp *regexp.Regexp
+	guestDNSNotReadyRegexp                 = regexp.MustCompile(guestDNSNotReadyPattern)
+	guestEOFRegexp                         = regexp.MustCompile(guestEOFPattern)
+	guestTransientInvalidCertificateRegexp = regexp.MustCompile(guestTransientInvalidCertificatePattern)
 )
 
-func init() {
-	guestDNSNotReadyRegexp = regexp.MustCompile(guestDNSNotReadyPattern)
-	guestTransientInvalidCertificateRegexp = regexp.MustCompile(guestTransientInvalidCertificatePattern)
-}
-
-var guestAPINotAvailableError = microerror.New("Guest API not available")
+var guestAPINotAvailableError = microerror.New("guest API not available")
 
 // IsGuestAPINotAvailable asserts guestAPINotAvailableError.
 func IsGuestAPINotAvailable(err error) bool {
@@ -48,6 +55,7 @@ func IsGuestAPINotAvailable(err error) bool {
 
 	regexps := []*regexp.Regexp{
 		guestDNSNotReadyRegexp,
+		guestEOFRegexp,
 		guestTransientInvalidCertificateRegexp,
 	}
 	for _, re := range regexps {
