47f7a8b9383c92864461af11f2c4befb1b001b8f
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-06-20 19:14:37 +0300

    Fix dependencies (#497)

--- flag/service/guest/guest.go
+++ flag/service/guest/guest.go
@@ -1,7 +1,7 @@
 package guest
 
 import (
-	"github.com/giantswarm/aws-operator/flag/service/guest/ssh"
+	"github.com/giantswarm/kvm-operator/flag/service/guest/ssh"
 	"github.com/giantswarm/kvm-operator/flag/service/guest/update"
 )
 
--- vendor/github.com/giantswarm/aws-operator/flag/service/guest/ssh/ssh.go
+++
@@ -1,5 +0,0 @@
-package ssh
-
-type SSH struct {
-	SSOPublicKey string
-}
