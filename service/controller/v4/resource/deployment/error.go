31e96d3bd3b0349a7294e975ca138b3e5cbdae30
Tim Schindler <xh3b4sd@gmail.com>
2018-07-31 09:58:19 +0200

    rewrite errors (#524)

--- service/controller/v4/resource/deployment/error.go
+++ service/controller/v4/resource/deployment/error.go
@@ -2,28 +2,36 @@ package deployment
 
 import "github.com/giantswarm/microerror"
 
-var executionFailedError = microerror.New("execution failed")
+var executionFailedError = &microerror.Error{
+	Kind: "executionFailedError",
+}
 
 // IsExecutionFailed asserts executionFailedError.
 func IsExecutionFailed(err error) bool {
 	return microerror.Cause(err) == executionFailedError
 }
 
-var invalidConfigError = microerror.New("invalid config")
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
-var notFoundError = microerror.New("not found")
+var notFoundError = &microerror.Error{
+	Kind: "notFoundError",
+}
 
 // IsNotFound asserts notFoundError.
 func IsNotFound(err error) bool {
 	return microerror.Cause(err) == notFoundError
 }
 
-var wrongTypeError = microerror.New("wrong type")
+var wrongTypeError = &microerror.Error{
+	Kind: "wrongTypeError",
+}
 
 // IsWrongTypeError asserts wrongTypeError.
 func IsWrongTypeError(err error) bool {
---
+++ vendor/github.com/giantswarm/microerror/error.go
@@ -0,0 +1,47 @@
+package microerror
+
+import "encoding/json"
+
+// Error is a predefined error structure whose purpose is to act as container
+// for meta information associated to a specific error. The specific error type
+// matching can be used as usual. The usual error masking and cause gathering
+// can be used as usual. Using Error might look as follows. In the beginning is
+// a usual error defined, along with its matcher. This error is the root cause
+// once emitted during runtime.
+//
+//     var notEnoughWorkersError = &microerror.Error{
+//         Desc: "The amount of requested guest cluster workers exceeds the available number of host cluster nodes.",
+//         Docs: "https://github.com/giantswarm/ops-recipes/blob/master/349-not-enough-workers.md",
+//         Kind: "notEnoughWorkersError",
+//     }
+//
+//     func IsNotEnoughWorkers(err error) bool {
+//         return microerror.Cause(err) == notEnoughWorkersError
+//     }
+//
+type Error struct {
+	Desc string `json:"desc"`
+	Docs string `json:"docs"`
+	Kind string `json:"kind"`
+}
+
+func (e *Error) Error() string {
+	if e.Desc == "" {
+		return e.String()
+	} else {
+		return e.Desc
+	}
+}
+
+func (e *Error) GoString() string {
+	return e.String()
+}
+
+func (e *Error) String() string {
+	b, err := json.Marshal(e)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(b)
+}
