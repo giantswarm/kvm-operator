2a084a692b6c439bd5208a67fbcd3f7c8513f865
Vaclav Rozsypalek <rozsypalek.vaclav@gmail.com>
2018-08-09 11:19:42 +0200

    tear down stuff

--- integration/teardown/teardown.go
+++ integration/teardown/teardown.go
@@ -13,20 +13,28 @@ import (
 // Teardown e2e testing environment.
 func Teardown(g *framework.Guest, h *framework.Host) error {
 	var err error
+	targetNamespace = env.ClusterID()
 
 	{
-		err = framework.HelmCmd(fmt.Sprintf("delete kvm-operator --namespace %s --purge", env.ClusterID()))
+		err = framework.HelmCmd(fmt.Sprintf("delete kvm-operator --namespace %s --purge", targetNamespace))
 		if err != nil {
 			return microerror.Mask(err)
 		}
-		err = framework.HelmCmd(fmt.Sprintf("delete cert-operator --namespace %s --purge", env.ClusterID()))
+		err = framework.HelmCmd(fmt.Sprintf("delete cert-operator --namespace %s --purge", targetNamespace))
 		if err != nil {
 			return microerror.Mask(err)
 		}
 	}
 
 	{
-		err = framework.HelmCmd(fmt.Sprintf("delete cert-config-e2e --namespace %s --purge", env.ClusterID()))
+		err = framework.HelmCmd(fmt.Sprintf("delete cert-config-e2e --namespace %s --purge", targetNamespace))
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		err = h.K8sClient().CoreV1().Namespaces().Delete(targetNamespace, &metav1.DeleteOptions{})
 		if err != nil {
 			return microerror.Mask(err)
 		}
