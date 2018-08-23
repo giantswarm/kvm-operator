94fdb92dd62315d294e9afd34108eb4a52ca66bf
calvix <rozsypalek.vaclav@gmail.com>
2018-08-23 14:13:48 +0200

    enable-crd-label-selector (#532)
    * enable-crd-label-selector
    
    * fix imports
    
    * set flag
    
    * add extra line

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,12 +1,14 @@
 package service
 
 import (
+	"github.com/giantswarm/kvm-operator/flag/service/crd"
 	"github.com/giantswarm/kvm-operator/flag/service/guest"
 	"github.com/giantswarm/kvm-operator/flag/service/installation"
 	"github.com/giantswarm/kvm-operator/flag/service/kubernetes"
 )
 
 type Service struct {
+	CRD          crd.CRD
 	Guest        guest.Guest
 	Installation installation.Installation
 	Kubernetes   kubernetes.Kubernetes
