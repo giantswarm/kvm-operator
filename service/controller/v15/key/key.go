96155e467b3479419f4d84c9f36037acdf6c7e9e
Tim Schindler <xh3b4sd@gmail.com>
2018-09-10 10:51:46 +0200

    sets proper docker image tag

--- service/controller/v15/key/key.go
+++ service/controller/v15/key/key.go
@@ -41,10 +41,8 @@ const (
 	CoreosVersion        = "1688.5.3"
 
 	K8SEndpointUpdaterDocker = "quay.io/giantswarm/k8s-endpoint-updater:df982fc73b71e60fc70a7444c068b52441ddb30e"
-	// TODO use the right tag once https://github.com/giantswarm/k8s-kvm/pull/22
-	// is merged.
-	K8SKVMDockerImage  = "quay.io/giantswarm/k8s-kvm:fc3bbd9cf5fef4e8df7a29d0441726778ebdeffe"
-	K8SKVMHealthDocker = "quay.io/giantswarm/k8s-kvm-health:ddf211dfed52086ade32ab8c45e44eb0273319ef"
+	K8SKVMDockerImage        = "quay.io/giantswarm/k8s-kvm:db5f9c0ad08fd99a7e775e31fba99e5c7f87ab59"
+	K8SKVMHealthDocker       = "quay.io/giantswarm/k8s-kvm-health:ddf211dfed52086ade32ab8c45e44eb0273319ef"
 
 	// constants for calculation qemu memory overhead.
 	baseMasterMemoryOverhead     = "1G"
