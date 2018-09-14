96e8502528bc3e45ed7d7ba161bdf27e732567ab
Tim Schindler <xh3b4sd@gmail.com>
2018-09-14 13:04:04 +0200

    copies v15 to v16 (#553)

---
+++ service/controller/v16/resource/endpoint/desired.go
@@ -0,0 +1,28 @@
+package endpoint
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	pod, err := toPod(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	endpointIP, serviceName, err := getAnnotations(*pod, IPAnnotation, ServiceAnnotation)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredEndpoint := &Endpoint{
+		IPs: []string{
+			endpointIP,
+		},
+		ServiceName:      serviceName,
+		ServiceNamespace: pod.GetNamespace(),
+	}
+
+	return desiredEndpoint, nil
+}
---
+++ service/controller/v16/version_bundle.go
@@ -0,0 +1,55 @@
+package v15
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "kubernetes",
+				Description: "Updated to 1.11.1.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Removed nginx-ingress-controller related components (will be managed by chart-operator).",
+				Kind:        versionbundle.KindRemoved,
+			},
+			{
+				Component:   "kvm-operator",
+				Description: "Made worker node docker volume size to be configurable.",
+				Kind:        versionbundle.KindChanged,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.0.5",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "1688.5.3",
+			},
+			{
+				Name:    "docker",
+				Version: "17.12.1",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.3",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.1.1",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.11.1",
+			},
+		},
+		Name:    "kvm-operator",
+		Version: "3.0.0",
+	}
+}
