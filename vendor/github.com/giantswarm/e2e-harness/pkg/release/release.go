d8f6a1672c05162fd9d11f2eb96b6e01fd0a7723
Tim Schindler <xh3b4sd@gmail.com>
2018-10-19 15:18:56 +0200

    updates vendor deps

--- vendor/github.com/giantswarm/e2e-harness/pkg/release/release.go
+++ vendor/github.com/giantswarm/e2e-harness/pkg/release/release.go
@@ -126,44 +126,53 @@ func (r *Release) Condition() ConditionSet {
 }
 
 func (r *Release) Delete(ctx context.Context, name string) error {
-	err := r.helmClient.DeleteRelease(name, helm.DeletePurge(true))
+	releaseName := fmt.Sprintf("%s-%s", r.namespace, name)
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting release %#q", releaseName))
+
+	err := r.helmClient.DeleteRelease(releaseName, helm.DeletePurge(true))
 	if helmclient.IsReleaseNotFound(err) {
-		return microerror.Maskf(releaseNotFoundError, name)
+		return microerror.Maskf(releaseNotFoundError, releaseName)
 	} else if helmclient.IsTillerNotFound(err) {
 		return microerror.Mask(tillerNotFoundError)
 	} else if err != nil {
 		return microerror.Mask(err)
 	}
 
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleted release %#q", releaseName))
+
 	return nil
 }
 
 func (r *Release) EnsureDeleted(ctx context.Context, name string) error {
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("ensuring deletion of release %#q", name))
+	releaseName := fmt.Sprintf("%s-%s", r.namespace, name)
 
-	err := r.helmClient.DeleteRelease(name, helm.DeletePurge(true))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("ensuring deletion of release %#q", releaseName))
+
+	err := r.helmClient.DeleteRelease(releaseName, helm.DeletePurge(true))
 	if helmclient.IsReleaseNotFound(err) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q does not exist", name))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q does not exist", releaseName))
 	} else if helmclient.IsTillerNotFound(err) {
 		r.logger.LogCtx(ctx, "level", "warning", "message", "tiller is not found/installed")
 	} else if err != nil {
 		return microerror.Mask(err)
 	} else {
-		r.logger.LogCtx(ctx, "level", "info", "message", fmt.Sprintf("deleted release %#q", name))
+		r.logger.LogCtx(ctx, "level", "info", "message", fmt.Sprintf("deleted release %#q", releaseName))
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("ensured deletion of release %#q", name))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("ensured deletion of release %#q", releaseName))
 
 	return nil
 }
 
 func (r *Release) Install(ctx context.Context, name string, version Version, values string, conditions ...func() error) error {
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("creating release %#q", name))
+	releaseName := fmt.Sprintf("%s-%s", r.namespace, name)
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("creating release %#q", releaseName))
 
 	var err error
 
 	chartname := fmt.Sprintf("%s-chart", name)
-	releaseName := fmt.Sprintf("%s-%s", r.namespace, name)
 
 	var tarball string
 	if version.isChannel {
@@ -190,7 +199,7 @@ func (r *Release) Install(ctx context.Context, name string, version Version, val
 		}
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("created release %#q", name))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("created release %#q", releaseName))
 
 	return nil
 }
