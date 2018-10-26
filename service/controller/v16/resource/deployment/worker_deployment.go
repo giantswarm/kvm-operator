76e3456af7c8bccbfadf548b229318b6566ed521
Tuomas Makinen <tuommaki@gmail.com>
2018-10-26 14:55:29 +0300

    Fix qemu-shutdown parameter with shutdown-deferrer query path

--- service/controller/v16/resource/deployment/worker_deployment.go
+++ service/controller/v16/resource/deployment/worker_deployment.go
@@ -217,7 +217,7 @@ func newWorkerDeployments(customObject v1alpha1.KVMConfig) ([]*extensionsv1.Depl
 								Lifecycle: &apiv1.Lifecycle{
 									PreStop: &apiv1.Handler{
 										Exec: &apiv1.ExecAction{
-											Command: []string{"/qemu-shutdown", key.ShutdownDeferrerListenAddress(customObject)},
+											Command: []string{"/qemu-shutdown", fmt.Sprintf("%s/v1/defer/", key.ShutdownDeferrerListenAddress(customObject))},
 										},
 									},
 								},
