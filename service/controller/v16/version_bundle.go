305f4497100353e1acb40284cb3f07106d9d02c3
Timo Derstappen <teemow@gmail.com>
2018-11-05 15:01:01 +0100

    add k8s 1.12.1 to V16 (#602)
    * upgrading cloudconfig to 3.7.0
      * upgrading k8s to 1.12.2
      * upgrading etcd to 3.3.9
    * upgrading container linux to 1855.5.0
      * upgrading docker to 18.06.1
    * adding a changelog
    * increment the kvm operator version to 3.1.0

--- service/controller/v16/version_bundle.go
+++ service/controller/v16/version_bundle.go
@@ -8,8 +8,38 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "TODO",
-				Description: "TODO",
+				Component:   "kubernetes",
+				Description: "Updated Kubernetes to 1.12.2. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.12.md",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "calico",
+				Description: "Updated to 3.2.3. Also the manifest has proper resource limits to get QoS policy guaranteed.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "kvm-operator",
+				Description: "Adapted limit calculation for very big kvm pods.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "kubernetes",
+				Description: "Enabled admission plugins: DefaultTolerationSeconds, MutatingAdmissionWebhook, ValidatingAdmissionWebhook.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "container-linux",
+				Description: "Updated to latest stable 1855.5.0",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "etcd",
+				Description: "Updated to 3.3.9",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "docker",
+				Description: "Updated to 18.06.1",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
@@ -20,15 +50,15 @@ func VersionBundle() versionbundle.Bundle {
 			},
 			{
 				Name:    "containerlinux",
-				Version: "1688.5.3",
+				Version: "1855.5.0",
 			},
 			{
 				Name:    "docker",
-				Version: "17.12.1",
+				Version: "18.06.1",
 			},
 			{
 				Name:    "etcd",
-				Version: "3.3.8",
+				Version: "3.3.9",
 			},
 			{
 				Name:    "coredns",
@@ -36,10 +66,10 @@ func VersionBundle() versionbundle.Bundle {
 			},
 			{
 				Name:    "kubernetes",
-				Version: "1.11.1",
+				Version: "1.12.2",
 			},
 		},
 		Name:    "kvm-operator",
-		Version: "3.0.1",
+		Version: "3.1.0",
 	}
 }
