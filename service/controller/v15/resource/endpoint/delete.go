fe029f9f231d1c4eb3e217bfd40629e01c7264c6
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-06 10:34:44 +0200

    Update service/controller/v15/resource/endpoint/delete.go
    Co-Authored-By: tuommaki <1947505+tuommaki@users.noreply.github.com>

--- service/controller/v15/resource/endpoint/delete.go
+++ service/controller/v15/resource/endpoint/delete.go
@@ -82,7 +82,7 @@ func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desir
 		} else if err != nil {
 			return nil, microerror.Mask(err)
 		} else {
-			r.logger.LogCtx(ctx, "level", "debug", "message", "found the current version of the reconciled pod in the Kubernetes API")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "found current version of the reconciled pod in the Kubernetes API")
 
 			if !key.ArePodContainersTerminated(currentPod) {
 				r.logger.LogCtx(ctx, "level", "debug", "message", "pod containers are still running")
