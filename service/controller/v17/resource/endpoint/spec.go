cc0c907ad26d987688ef4bcbc82e8e9a8f2f6681
Tim Schindler <xh3b4sd@gmail.com>
2019-01-15 22:37:06 +0100

    improves todo

--- service/controller/v17/resource/endpoint/spec.go
+++ service/controller/v17/resource/endpoint/spec.go
@@ -4,8 +4,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// TODO Addresses and IPs are basically the same and we only need one data type.
-// The whole thing could be refactored and simplified a little bit.
+// TODO Addresses and IPs are basically the same and we only need Addresses in
+// fact. The whole thing could be refactored and simplified a little bit later.
 type Endpoint struct {
 	Addresses        []corev1.EndpointAddress
 	Ports            []corev1.EndpointPort
