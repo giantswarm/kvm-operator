3049d8ebbe8c7ad8110ad2f3235a20e7d8438e77
Tim Schindler <xh3b4sd@gmail.com>
2019-01-22 17:39:16 +0100

    adds TODO comment

--- service/controller/v18/resource/endpoint/spec.go
+++ service/controller/v18/resource/endpoint/spec.go
@@ -6,6 +6,9 @@ import (
 
 // TODO Addresses and IPs are basically the same and we only need Addresses in
 // fact. The whole thing could be refactored and simplified a little bit later.
+//
+//     https://github.com/giantswarm/giantswarm/issues/5154
+//
 type Endpoint struct {
 	Addresses        []corev1.EndpointAddress
 	Ports            []corev1.EndpointPort
