2c721243fc34fdeb07489d025ca31279e3b23811
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-02-13 13:27:55 +0100

    v19: copy v18

---
+++ service/controller/v19/version_bundle.go
@@ -0,0 +1,66 @@
+package v18
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "kvm-operator",
+				Description: "Add k8s api and kubelet health check to kvm pod.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Switch from cloudinit to ignition.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Update tenant cluster container with Fedora 29.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "node-operator",
+				Description: "Improved node draining during updates and scaling.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Add static iSCSI initiator name for each vm.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "containerlinux",
+				Description: "Update to 1967.5.0 (CVE-2019-5736).",
+				Kind:        versionbundle.KindChanged,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.2.3",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "1911.5.0",
+			},
+			{
+				Name:    "docker",
+				Version: "18.06.1",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.9",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.12.3",
+			},
+		},
+		Name:    "kvm-operator",
+		Version: "3.2.0",
+	}
+}
