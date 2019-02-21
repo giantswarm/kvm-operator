073a33587ddd09b5f4278ee5b7ed3c520d6664fc
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-02-21 14:40:50 +0100

    order

--- service/controller/v17patch1/resource/endpoint/current.go
+++ service/controller/v17patch1/resource/endpoint/current.go
@@ -2,14 +2,15 @@ package endpoint
 
 import (
 	"context"
-	"github.com/giantswarm/kvm-operator/service/controller/v17patch1/key"
-	"github.com/giantswarm/operatorkit/controller/context/finalizerskeptcontext"
 
 	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller/context/finalizerskeptcontext"
 	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/kvm-operator/service/controller/v17patch1/key"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
