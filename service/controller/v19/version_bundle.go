e6d7b527185869528c77b5d5ae618811ec8e90e6
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2019-03-04 15:18:34 +0200

    Wire up v19 versionbundle (#670)

--- service/controller/v19/version_bundle.go
+++ service/controller/v19/version_bundle.go
@@ -9,53 +9,8 @@ func VersionBundle() versionbundle.Bundle {
 		Changelogs: []versionbundle.Changelog{
 			{
 				Component:   "kvm-operator",
-				Description: "Add k8s api and kubelet health check to kvm pod.",
+				Description: "TODO",
 				Kind:        versionbundle.KindAdded,
-			},
-			{
-				Component:   "kvm-operator",
-				Description: "Add readiness probe to kvm pod to improve updates.",
-				Kind:        versionbundle.KindAdded,
-			},
-			{
-				Component:   "cloudconfig",
-				Description: "Switch from cloudinit to ignition.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "cloudconfig",
-				Description: "Update tenant cluster container with Fedora 29.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "node-operator",
-				Description: "Improved node draining during updates and scaling.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "cloudconfig",
-				Description: "Add static iSCSI initiator name for each vm.",
-				Kind:        versionbundle.KindAdded,
-			},
-			{
-				Component:   "containerlinux",
-				Description: "Update to 1967.5.0 (CVE-2019-5736).",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "kubernetes",
-				Description: "Updated kubernetes to 1.13.3.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "calico",
-				Description: "Updated calico to 3.5.1.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "etcd",
-				Description: "Updated calico to 3.3.12.",
-				Kind:        versionbundle.KindChanged,
 			},
 		},
 		Components: []versionbundle.Component{
@@ -81,6 +36,6 @@ func VersionBundle() versionbundle.Bundle {
 			},
 		},
 		Name:    "kvm-operator",
-		Version: "3.2.0",
+		Version: "3.3.0",
 	}
 }
