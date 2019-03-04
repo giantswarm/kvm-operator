45bdb78623ce5a73e2ff77e716dbcf0c0ec93309
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2019-03-04 08:45:39 +0200

    Simplify e2e setup with chartvalues & helm client library (#666)
    This change removes old mechanism to install helm charts by executing helm
    command and uses e2e-harness EnsureInstalled() & EnsureDeleted() functions
    instead. These provide better error handling and cleaner interface.

--- integration/key/key.go
+++ integration/key/key.go
@@ -16,6 +16,14 @@ func ClusterRolePSP(operator string) string {
 	return fmt.Sprintf("%s-%s-psp", env.ClusterID(), operator)
 }
 
+func FlannelReleaseName(namespace string) string {
+	return fmt.Sprintf("%s-flannel-config-e2e", namespace)
+}
+
+func KVMReleaseName(namespace string) string {
+	return fmt.Sprintf("%s-kvm-config-e2e", namespace)
+}
+
 func LabelSelector() string {
 	return fmt.Sprintf("giantswarm.io/cluster=%s", env.ClusterID())
 }
