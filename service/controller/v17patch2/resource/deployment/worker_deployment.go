520ed587f9f1ab1eee0b239cc455f10ea61ab6b4
Ferran <ferrandinand@users.noreply.github.com>
2019-03-05 14:53:18 +0100

    Added v17patch2 copy (#677)

---
+++ service/controller/v17patch2/resource/deployment/worker_deployment.go
@@ -0,0 +1,362 @@
+package deployment
+
+import (
+	"fmt"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/kvm-operator/service/controller/v17patch1/key"
+	"github.com/giantswarm/microerror"
+	apiv1 "k8s.io/api/core/v1"
+	extensionsv1 "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func newWorkerDeployments(customResource v1alpha1.KVMConfig) ([]*extensionsv1.Deployment, error) {
+	var deployments []*extensionsv1.Deployment
+
+	privileged := true
+	replicas := int32(1)
+	podDeletionGracePeriod := int64(key.PodDeletionGracePeriod.Seconds())
+
+	for i, workerNode := range customResource.Spec.Cluster.Workers {
+		capabilities := customResource.Spec.KVM.Workers[i]
+
+		cpuQuantity, err := key.CPUQuantity(capabilities)
+		if err != nil {
+			return nil, microerror.Maskf(err, "creating CPU quantity")
+		}
+
+		memoryQuantity, err := key.MemoryQuantityWorker(capabilities)
+		if err != nil {
+			return nil, microerror.Maskf(err, "creating memory quantity")
+		}
+
+		deployment := &extensionsv1.Deployment{
+			TypeMeta: apismetav1.TypeMeta{
+				Kind:       "deployment",
+				APIVersion: "extensions/v1beta",
+			},
+			ObjectMeta: apismetav1.ObjectMeta{
+				Name: key.DeploymentName(key.WorkerID, workerNode.ID),
+				Annotations: map[string]string{
+					key.VersionBundleVersionAnnotation: key.VersionBundleVersion(customResource),
+				},
+				Labels: map[string]string{
+					key.LabelApp:          key.WorkerID,
+					"cluster":             key.ClusterID(customResource),
+					"customer":            key.ClusterCustomer(customResource),
+					key.LabelCluster:      key.ClusterID(customResource),
+					key.LabelOrganization: key.ClusterCustomer(customResource),
+					key.LabelManagedBy:    key.OperatorName,
+					"node":                workerNode.ID,
+				},
+			},
+			Spec: extensionsv1.DeploymentSpec{
+				Selector: &apismetav1.LabelSelector{
+					MatchLabels: map[string]string{
+						key.LabelApp: key.WorkerID,
+						"cluster":    key.ClusterID(customResource),
+						"node":       workerNode.ID,
+					},
+				},
+				Strategy: extensionsv1.DeploymentStrategy{
+					Type: extensionsv1.RecreateDeploymentStrategyType,
+				},
+				Replicas: &replicas,
+				Template: apiv1.PodTemplateSpec{
+					ObjectMeta: apismetav1.ObjectMeta{
+						Annotations: map[string]string{
+							key.AnnotationAPIEndpoint:   key.ClusterAPIEndpoint(customResource),
+							key.AnnotationIp:            "",
+							key.AnnotationService:       key.WorkerID,
+							key.AnnotationPodDrained:    "False",
+							key.AnnotationVersionBundle: key.VersionBundleVersion(customResource),
+						},
+						Name: key.WorkerID,
+						Labels: map[string]string{
+							key.LabelApp:          key.WorkerID,
+							"cluster":             key.ClusterID(customResource),
+							"customer":            key.ClusterCustomer(customResource),
+							key.LabelCluster:      key.ClusterID(customResource),
+							key.LabelOrganization: key.ClusterCustomer(customResource),
+							"node":                workerNode.ID,
+							key.PodWatcherLabel:   "kvm-operator",
+						},
+					},
+					Spec: apiv1.PodSpec{
+						Affinity:    newWorkerPodAfinity(customResource),
+						HostNetwork: true,
+						NodeSelector: map[string]string{
+							"role": key.WorkerID,
+						},
+						ServiceAccountName:            key.ServiceAccountName(customResource),
+						TerminationGracePeriodSeconds: &podDeletionGracePeriod,
+						Volumes: []apiv1.Volume{
+							{
+								Name: "cloud-config",
+								VolumeSource: apiv1.VolumeSource{
+									ConfigMap: &apiv1.ConfigMapVolumeSource{
+										LocalObjectReference: apiv1.LocalObjectReference{
+											Name: key.ConfigMapName(customResource, workerNode, key.WorkerID),
+										},
+									},
+								},
+							},
+							{
+								Name: "images",
+								VolumeSource: apiv1.VolumeSource{
+									HostPath: &apiv1.HostPathVolumeSource{
+										Path: key.CoreosImageDir,
+									},
+								},
+							},
+							{
+								Name: "rootfs",
+								VolumeSource: apiv1.VolumeSource{
+									EmptyDir: &apiv1.EmptyDirVolumeSource{},
+								},
+							},
+							{
+								Name: "flannel",
+								VolumeSource: apiv1.VolumeSource{
+									HostPath: &apiv1.HostPathVolumeSource{
+										Path: key.FlannelEnvPathPrefix,
+									},
+								},
+							},
+						},
+						Containers: []apiv1.Container{
+							{
+								Name:            "k8s-endpoint-updater",
+								Image:           key.K8SEndpointUpdaterDocker,
+								ImagePullPolicy: apiv1.PullIfNotPresent,
+								Command: []string{
+									"/opt/k8s-endpoint-updater",
+									"update",
+									"--provider.bridge.name=" + key.NetworkBridgeName(customResource),
+									"--service.kubernetes.cluster.namespace=" + key.ClusterNamespace(customResource),
+									"--service.kubernetes.cluster.service=" + key.WorkerID,
+									"--service.kubernetes.inCluster=true",
+								},
+								SecurityContext: &apiv1.SecurityContext{
+									Privileged: &privileged,
+								},
+								Env: []apiv1.EnvVar{
+									{
+										Name: "POD_NAME",
+										ValueFrom: &apiv1.EnvVarSource{
+											FieldRef: &apiv1.ObjectFieldSelector{
+												APIVersion: "v1",
+												FieldPath:  "metadata.name",
+											},
+										},
+									},
+								},
+							},
+							{
+								Name:            "k8s-kvm",
+								Image:           key.K8SKVMDockerImage,
+								ImagePullPolicy: apiv1.PullIfNotPresent,
+								SecurityContext: &apiv1.SecurityContext{
+									Privileged: &privileged,
+								},
+								Args: []string{
+									key.WorkerID,
+								},
+								Env: []apiv1.EnvVar{
+									{
+										Name:  "CORES",
+										Value: fmt.Sprintf("%d", capabilities.CPUs),
+									},
+									{
+										Name:  "COREOS_VERSION",
+										Value: key.CoreosVersion,
+									},
+									{
+										Name:  "DISK_DOCKER",
+										Value: key.DockerVolumeSizeFromNode(capabilities),
+									},
+									{
+										Name:  "DISK_KUBELET",
+										Value: key.KubeletVolumeSizeFromNode(capabilities),
+									},
+									{
+										Name:  "DISK_OS",
+										Value: key.DefaultOSDiskSize,
+									},
+									{
+										Name: "HOSTNAME",
+										ValueFrom: &apiv1.EnvVarSource{
+											FieldRef: &apiv1.ObjectFieldSelector{
+												APIVersion: "v1",
+												FieldPath:  "metadata.name",
+											},
+										},
+									},
+									{
+										Name:  "NETWORK_BRIDGE_NAME",
+										Value: key.NetworkBridgeName(customResource),
+									},
+									{
+										Name:  "NETWORK_TAP_NAME",
+										Value: key.NetworkTapName(customResource),
+									},
+									{
+										Name: "MEMORY",
+										// TODO provide memory like disk as float64 and format here.
+										Value: capabilities.Memory,
+									},
+									{
+										Name:  "ROLE",
+										Value: key.WorkerID,
+									},
+									{
+										Name:  "CLOUD_CONFIG_PATH",
+										Value: "/cloudconfig/user_data",
+									},
+								},
+								Lifecycle: &apiv1.Lifecycle{
+									PreStop: &apiv1.Handler{
+										Exec: &apiv1.ExecAction{
+											Command: []string{"/qemu-shutdown", key.ShutdownDeferrerPollPath(customResource)},
+										},
+									},
+								},
+								LivenessProbe: &apiv1.Probe{
+									InitialDelaySeconds: key.LivenessProbeInitialDelaySeconds,
+									TimeoutSeconds:      key.TimeoutSeconds,
+									PeriodSeconds:       key.PeriodSeconds,
+									FailureThreshold:    key.FailureThreshold,
+									SuccessThreshold:    key.SuccessThreshold,
+									Handler: apiv1.Handler{
+										HTTPGet: &apiv1.HTTPGetAction{
+											Path: key.HealthEndpoint,
+											Port: intstr.IntOrString{IntVal: key.LivenessPort(customResource)},
+											Host: key.ProbeHost,
+										},
+									},
+								},
+								ReadinessProbe: &apiv1.Probe{
+									InitialDelaySeconds: key.ReadinessProbeInitialDelaySeconds,
+									TimeoutSeconds:      key.TimeoutSeconds,
+									PeriodSeconds:       key.PeriodSeconds,
+									FailureThreshold:    key.FailureThreshold,
+									SuccessThreshold:    key.SuccessThreshold,
+									Handler: apiv1.Handler{
+										HTTPGet: &apiv1.HTTPGetAction{
+											Path: key.HealthEndpoint,
+											Port: intstr.IntOrString{IntVal: key.LivenessPort(customResource)},
+											Host: key.ProbeHost,
+										},
+									},
+								},
+								Resources: apiv1.ResourceRequirements{
+									Requests: map[apiv1.ResourceName]resource.Quantity{
+										apiv1.ResourceCPU:    cpuQuantity,
+										apiv1.ResourceMemory: memoryQuantity,
+									},
+									Limits: map[apiv1.ResourceName]resource.Quantity{
+										apiv1.ResourceCPU:    cpuQuantity,
+										apiv1.ResourceMemory: memoryQuantity,
+									},
+								},
+								VolumeMounts: []apiv1.VolumeMount{
+									{
+										Name:      "cloud-config",
+										MountPath: "/cloudconfig/",
+									},
+									{
+										Name:      "images",
+										MountPath: "/usr/code/images/",
+									},
+									{
+										Name:      "rootfs",
+										MountPath: "/usr/code/rootfs/",
+									},
+								},
+							},
+							{
+								Name:            "k8s-kvm-health",
+								Image:           key.K8SKVMHealthDocker,
+								ImagePullPolicy: apiv1.PullAlways,
+								Env: []apiv1.EnvVar{
+									{
+										Name:  "LISTEN_ADDRESS",
+										Value: key.HealthListenAddress(customResource),
+									},
+									{
+										Name:  "NETWORK_ENV_FILE_PATH",
+										Value: key.NetworkEnvFilePath(customResource),
+									},
+								},
+								SecurityContext: &apiv1.SecurityContext{
+									Privileged: &privileged,
+								},
+								VolumeMounts: []apiv1.VolumeMount{
+									{
+										Name:      "flannel",
+										MountPath: key.FlannelEnvPathPrefix,
+									},
+								},
+							},
+							{
+								Name:            "shutdown-deferrer",
+								Image:           key.ShutdownDeferrerDocker,
+								ImagePullPolicy: apiv1.PullAlways,
+								Args: []string{
+									"daemon",
+									"--server.listen.address=" + key.ShutdownDeferrerListenAddress(customResource),
+								},
+								Env: []apiv1.EnvVar{
+									{
+										Name: key.EnvKeyMyPodName,
+										ValueFrom: &apiv1.EnvVarSource{
+											FieldRef: &apiv1.ObjectFieldSelector{
+												FieldPath: "metadata.name",
+											},
+										},
+									},
+									{
+										Name: key.EnvKeyMyPodNamespace,
+										ValueFrom: &apiv1.EnvVarSource{
+											FieldRef: &apiv1.ObjectFieldSelector{
+												FieldPath: "metadata.namespace",
+											},
+										},
+									},
+								},
+								Lifecycle: &apiv1.Lifecycle{
+									PreStop: &apiv1.Handler{
+										Exec: &apiv1.ExecAction{
+											Command: []string{"/pre-shutdown-hook", key.ShutdownDeferrerPollPath(customResource)},
+										},
+									},
+								},
+								LivenessProbe: &apiv1.Probe{
+									InitialDelaySeconds: key.LivenessProbeInitialDelaySeconds,
+									TimeoutSeconds:      key.TimeoutSeconds,
+									PeriodSeconds:       key.PeriodSeconds,
+									FailureThreshold:    key.FailureThreshold,
+									SuccessThreshold:    key.SuccessThreshold,
+									Handler: apiv1.Handler{
+										HTTPGet: &apiv1.HTTPGetAction{
+											Path: key.HealthEndpoint,
+											Port: intstr.IntOrString{IntVal: int32(key.ShutdownDeferrerListenPort(customResource))},
+											Host: key.ProbeHost,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		deployments = append(deployments, deployment)
+	}
+
+	return deployments, nil
+}
---
+++ service/controller/v17patch2/resource/serviceaccount/resource.go
@@ -0,0 +1,70 @@
+package serviceaccount
+
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	// Name is the identifier of the resource.
+	Name = "serviceaccountv17patch1"
+)
+
+// Config represents the configuration used to create a new cloud config resource.
+type Config struct {
+	// Dependencies.
+	K8sClient kubernetes.Interface
+	Logger    micrologger.Logger
+}
+
+// DefaultConfig provides a default configuration to create a new config map
+// resource by best effort.
+func DefaultConfig() Config {
+	return Config{
+		K8sClient: nil,
+		Logger:    nil,
+	}
+}
+
+// Resource implements the config map resource.
+type Resource struct {
+	// Dependencies.
+	k8sClient kubernetes.Interface
+	logger    micrologger.Logger
+}
+
+// New creates a new configured config map resource.
+func New(config Config) (*Resource, error) {
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.K8sClient must not be empty")
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+
+	newService := &Resource{
+		k8sClient: config.K8sClient,
+		logger:    config.Logger,
+	}
+
+	return newService, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func toServiceAccount(v interface{}) (*apiv1.ServiceAccount, error) {
+	if v == nil {
+		return nil, nil
+	}
+
+	serviceAccount, ok := v.(*apiv1.ServiceAccount)
+	if !ok {
+		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", apiv1.ServiceAccount{}, v)
+	}
+
+	return serviceAccount, nil
+}
