9b3340f1595e2b95ceaf78fbea7389ebdb1d4b6a
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-15 11:10:59 +0100

    Add kubeconfig support (#684)
    * vendor: updated operatorkit
    
    * Flags changes
    
    * Add missing setting
    
    * Fix TLS config struct
    
    * vendor: updated deps
    
    * Update tag for shutdown-deferrer image

--- service/controller/v19/key/key.go
+++ service/controller/v19/key/key.go
@@ -56,7 +56,7 @@ const (
 	K8SEndpointUpdaterDocker = "quay.io/giantswarm/k8s-endpoint-updater:590479a6228c2c143695a268bda5382b52f7ffe1"
 	K8SKVMDockerImage        = "quay.io/giantswarm/k8s-kvm:13b00b725fa2e5ed402f1f5973e1aaf14d193de2"
 	K8SKVMHealthDocker       = "quay.io/giantswarm/k8s-kvm-health:20517098a762a0d7ca2b0902316ddff487dbc7f5"
-	ShutdownDeferrerDocker   = "quay.io/giantswarm/shutdown-deferrer:b2ffdb2c4ec93fe6bf2d4af8e55c8a4b11253611"
+	ShutdownDeferrerDocker   = "quay.io/giantswarm/shutdown-deferrer:4e7d2b73859ea7dac1a2138e04e07fa5870d109b"
 
 	// constants for calculation qemu memory overhead.
 	baseMasterMemoryOverhead     = "1G"
