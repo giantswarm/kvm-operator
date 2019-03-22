85559a6d7467b69684b5371aa233699e1d6b9238
Joseph Salisbury <salisbury.joseph@gmail.com>
2019-03-22 11:32:18 +0000

    Updates k8s-kvm docker image with master branch

--- service/controller/v20/key/key.go
+++ service/controller/v20/key/key.go
@@ -54,7 +54,7 @@ const (
 	CoreosVersion        = "2023.4.0"
 
 	K8SEndpointUpdaterDocker = "quay.io/giantswarm/k8s-endpoint-updater:590479a6228c2c143695a268bda5382b52f7ffe1"
-	K8SKVMDockerImage        = "quay.io/giantswarm/k8s-kvm:c8ab6b741c7dfc6c4f92d8c698e0d6f46ec76c77"
+	K8SKVMDockerImage        = "quay.io/giantswarm/k8s-kvm:3c1d45c2a297fda0fd27c8d2506d8bf8edc96bc5"
 	K8SKVMHealthDocker       = "quay.io/giantswarm/k8s-kvm-health:20517098a762a0d7ca2b0902316ddff487dbc7f5"
 	ShutdownDeferrerDocker   = "quay.io/giantswarm/shutdown-deferrer:4e7d2b73859ea7dac1a2138e04e07fa5870d109b"
 
