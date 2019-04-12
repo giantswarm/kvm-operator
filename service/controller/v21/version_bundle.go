22a58b03f187d346ad5a0562080fe8bb0ea26aa4
Salvatore Mazzarino <dev@mazzarino.cz>
2019-04-12 09:35:47 +0200

    Create version bundle description

--- service/controller/v21/version_bundle.go
+++ service/controller/v21/version_bundle.go
@@ -12,11 +12,21 @@ func VersionBundle() versionbundle.Bundle {
 				Description: "Pin calico-kube-controllers to master.",
 				Kind:        versionbundle.KindChanged,
 			},
+			{
+				Component:   "kubernetes",
+				Description: "Update kubernetes to 1.14.1. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "calico",
+				Description: "Update calico to 3.6.1",
+				Kind:        versionbundle.KindChanged,
+			},
 		},
 		Components: []versionbundle.Component{
 			{
 				Name:    "calico",
-				Version: "3.5.1",
+				Version: "3.6.1",
 			},
 			{
 				Name:    "containerlinux",
@@ -32,7 +42,7 @@ func VersionBundle() versionbundle.Bundle {
 			},
 			{
 				Name:    "kubernetes",
-				Version: "1.13.4",
+				Version: "1.14.1",
 			},
 		},
 		Name:    "kvm-operator",
