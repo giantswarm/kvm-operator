944dfbe1987e9425f49cf7d2829cfe494d98466a
Tim Schindler <xh3b4sd@gmail.com>
2019-07-24 13:38:33 +0200

    align tests with fmt

--- service/controller/v24/resource/endpoint/update_test.go
+++ service/controller/v24/resource/endpoint/update_test.go
@@ -8,20 +8,20 @@ import (
 
 	g8sfake "github.com/giantswarm/apiextensions/pkg/clientset/versioned/fake"
 	"github.com/giantswarm/micrologger/microloggertest"
+	"github.com/google/go-cmp/cmp"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes/fake"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
 )
 
 func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 	testCases := []struct {
-		CurrentState        *Endpoint
-		DesiredState        *Endpoint
-		ExpectedCreateState *corev1.Endpoints
-		Obj                 interface{}
+		currentState *Endpoint
+		desiredState *Endpoint
+		updateChange *corev1.Endpoints
 	}{
 		{
-			CurrentState: &Endpoint{
+			currentState: &Endpoint{
 				IPs: []string{
 					"1.1.1.1",
 				},
@@ -33,14 +33,14 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			DesiredState: &Endpoint{
+			desiredState: &Endpoint{
 				IPs: []string{
 					"1.1.1.1",
 				},
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			ExpectedCreateState: &corev1.Endpoints{
+			updateChange: &corev1.Endpoints{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "v1",
 				},
@@ -65,7 +65,7 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 			},
 		},
 		{
-			CurrentState: &Endpoint{
+			currentState: &Endpoint{
 				IPs: []string{
 					"1.1.1.1",
 					"1.2.3.4",
@@ -78,14 +78,14 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			DesiredState: &Endpoint{
+			desiredState: &Endpoint{
 				IPs: []string{
 					"1.1.1.1",
 				},
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			ExpectedCreateState: &corev1.Endpoints{
+			updateChange: &corev1.Endpoints{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "v1",
 				},
@@ -113,7 +113,7 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 			},
 		},
 		{
-			CurrentState: &Endpoint{
+			currentState: &Endpoint{
 				IPs: []string{
 					"5.5.5.5",
 					"1.2.3.4",
@@ -126,14 +126,14 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			DesiredState: &Endpoint{
+			desiredState: &Endpoint{
 				IPs: []string{
 					"1.1.1.1",
 				},
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			ExpectedCreateState: &corev1.Endpoints{
+			updateChange: &corev1.Endpoints{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "v1",
 				},
@@ -164,7 +164,7 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 			},
 		},
 		{
-			CurrentState: &Endpoint{
+			currentState: &Endpoint{
 				Ports: []corev1.EndpointPort{
 					{
 						Port: 1234,
@@ -173,14 +173,14 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			DesiredState: &Endpoint{
+			desiredState: &Endpoint{
 				IPs: []string{
 					"1.1.1.1",
 				},
 				ServiceName:      "TestService",
 				ServiceNamespace: "TestNamespace",
 			},
-			ExpectedCreateState: &corev1.Endpoints{
+			updateChange: &corev1.Endpoints{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "v1",
 				},
@@ -205,26 +205,26 @@ func Test_Resource_Endpoint_newUpdateChange(t *testing.T) {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			var err error
 
-			var newResource *Resource
+			var r *Resource
 			{
 				c := Config{
 					G8sClient: g8sfake.NewSimpleClientset(),
-					K8sClient: fake.NewSimpleClientset(),
+					K8sClient: k8sfake.NewSimpleClientset(),
 					Logger:    microloggertest.New(),
 				}
 
-				newResource, err = New(c)
+				r, err = New(c)
 				if err != nil {
-					t.Fatal("expected", nil, "got", err)
+					t.Fatal(err)
 				}
 			}
 
-			result, err := newResource.newUpdateChange(context.TODO(), tc.Obj, tc.CurrentState, tc.DesiredState)
+			updateChange, err := r.newUpdateChange(context.Background(), nil, tc.currentState, tc.desiredState)
 			if err != nil {
-				t.Fatal("case", i, "expected", nil, "got", err)
+				t.Fatal(err)
 			}
-			if !reflect.DeepEqual(tc.ExpectedCreateState, result) {
-				t.Fatalf("case %d expected %#v got %#v", i, tc.ExpectedCreateState, result)
+			if !reflect.DeepEqual(tc.updateChange, updateChange) {
+				t.Fatalf("\n\n%s\n", cmp.Diff(updateChange, tc.updateChange))
 			}
 		})
 	}
