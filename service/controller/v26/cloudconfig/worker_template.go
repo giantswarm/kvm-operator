c2bbc8ee32e1e02877882e86d946cb829670e71f
Salvatore Mazzarino <dev@mazzarino.cz>
2019-11-12 12:10:39 +0100

    IPVS

--- service/controller/v26/cloudconfig/worker_template.go
+++ service/controller/v26/cloudconfig/worker_template.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/certs"
-	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_4_9_0"
+	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_5_0_0"
 	"github.com/giantswarm/microerror"
 
 	"github.com/giantswarm/kvm-operator/service/controller/v26/key"
