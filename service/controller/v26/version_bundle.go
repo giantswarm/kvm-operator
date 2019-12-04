5b37825834574f73fb4446a97406e682c2ded8ae
Thomas Fussell <thomas@fussell.io>
2019-12-04 08:17:04 -0500

    update changelog

--- service/controller/v26/version_bundle.go
+++ service/controller/v26/version_bundle.go
@@ -23,15 +23,47 @@ func VersionBundle() versionbundle.Bundle {
 					"https://github.com/giantswarm/k8scloudconfig/pull/609",
 				},
 			},
+			{
+				Component:   "calico",
+				Description: "Update from v3.9.1 to v3.10.1.",
+				Kind:        versionbundle.KindChanged,
+				URLs: []string{
+					"https://github.com/giantswarm/aws-operator/pull/2084",
+				},
+			},
+			{
+				Component:   "containerlinux",
+				Description: "Update from v2191.5.0 to v2247.6.0.",
+				Kind:        versionbundle.KindChanged,
+				URLs: []string{
+					"https://github.com/giantswarm/aws-operator/pull/2084",
+				},
+			},
+			{
+				Component:   "etcd",
+				Description: "Update from v3.3.15 to v3.3.17.",
+				Kind:        versionbundle.KindChanged,
+				URLs: []string{
+					"https://github.com/giantswarm/aws-operator/pull/2084",
+				},
+			},
+			{
+				Component:   "kubernetes",
+				Description: "Update from v1.15.5 to v1.16.3.",
+				Kind:        versionbundle.KindAdded,
+				URLs: []string{
+					"https://github.com/giantswarm/aws-operator/pull/2084",
+				},
+			},
 		},
 		Components: []versionbundle.Component{
 			{
 				Name:    "calico",
-				Version: "3.9.1",
+				Version: "3.10.1",
 			},
 			{
 				Name:    "containerlinux",
-				Version: "2191.5.0",
+				Version: "2247.6.0",
 			},
 			{
 				Name:    "docker",
@@ -39,11 +71,11 @@ func VersionBundle() versionbundle.Bundle {
 			},
 			{
 				Name:    "etcd",
-				Version: "3.3.15",
+				Version: "3.3.17",
 			},
 			{
 				Name:    "kubernetes",
-				Version: "1.15.5",
+				Version: "1.16.3",
 			},
 		},
 		Name:    "kvm-operator",
