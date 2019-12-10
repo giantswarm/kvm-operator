279f2db21341cf81990f99ff32e8e6ddd2074337
Salvatore Mazzarino <salvo@giantswarm.io>
2019-12-10 13:34:57 +0100

    Revert "Revert IPVS (#792)" (#794)
    This reverts commit aad58e1825370a503a1b84f613ebfdf65d8755de.

--- service/controller/v26/version_bundle.go
+++ service/controller/v26/version_bundle.go
@@ -16,6 +16,14 @@ func VersionBundle() versionbundle.Bundle {
 				},
 			},
 			{
+				Component:   "kube-proxy",
+				Description: "Switch from iptables to IPVS mode and tune kernel params accordingly.",
+				Kind:        versionbundle.KindChanged,
+				URLs: []string{
+					"https://github.com/giantswarm/k8scloudconfig/pull/604",
+				},
+			},
+			{
 				Component:   "kubernetes",
 				Description: "Add Deny All as default Network Policy in kube-system and giantswarm namespaces.",
 				Kind:        versionbundle.KindChanged,
