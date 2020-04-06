013a974996f43356dcd5e1f98bde3fab325a9bb8
Zach Stone <zach@giantswarm.io>
2020-04-06 11:52:01 +0200

    Correct ingress type

--- service/controller/resource/ingress/api_ingress.go
+++ service/controller/resource/ingress/api_ingress.go
@@ -2,15 +2,15 @@ package ingress
 
 import (
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
-	extensionsv1 "k8s.io/api/extensions/v1beta1"
+	networkingv1beta1 "k8s.io/api/networking/v1beta1"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
 	"github.com/giantswarm/kvm-operator/service/controller/key"
 )
 
-func newAPIIngress(customObject v1alpha1.KVMConfig) *extensionsv1.Ingress {
-	ingress := &extensionsv1.Ingress{
+func newAPIIngress(customObject v1alpha1.KVMConfig) *networkingv1beta1.Ingress {
+	ingress := &networkingv1beta1.Ingress{
 		TypeMeta: apismetav1.TypeMeta{
 			Kind:       "Ingress",
 			APIVersion: "extensions/v1beta",
@@ -26,23 +26,23 @@ func newAPIIngress(customObject v1alpha1.KVMConfig) *extensionsv1.Ingress {
 				"nginx.ingress.kubernetes.io/ssl-passthrough": "true",
 			},
 		},
-		Spec: extensionsv1.IngressSpec{
-			TLS: []extensionsv1.IngressTLS{
+		Spec: networkingv1beta1.IngressSpec{
+			TLS: []networkingv1beta1.IngressTLS{
 				{
 					Hosts: []string{
 						customObject.Spec.Cluster.Kubernetes.API.Domain,
 					},
 				},
 			},
-			Rules: []extensionsv1.IngressRule{
+			Rules: []networkingv1beta1.IngressRule{
 				{
 					Host: customObject.Spec.Cluster.Kubernetes.API.Domain,
-					IngressRuleValue: extensionsv1.IngressRuleValue{
-						HTTP: &extensionsv1.HTTPIngressRuleValue{
-							Paths: []extensionsv1.HTTPIngressPath{
+					IngressRuleValue: networkingv1beta1.IngressRuleValue{
+						HTTP: &networkingv1beta1.HTTPIngressRuleValue{
+							Paths: []networkingv1beta1.HTTPIngressPath{
 								{
 									Path: "/",
-									Backend: extensionsv1.IngressBackend{
+									Backend: networkingv1beta1.IngressBackend{
 										ServiceName: key.MasterID,
 										ServicePort: intstr.FromInt(customObject.Spec.Cluster.Kubernetes.API.SecurePort),
 									},
--- service/controller/resource/ingress/current.go
+++ service/controller/resource/ingress/current.go
@@ -7,7 +7,7 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/controller/context/finalizerskeptcontext"
 	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
-	"k8s.io/api/extensions/v1beta1"
+	"k8s.io/api/networking/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -31,7 +31,7 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interf
 	}
 
 	for _, name := range ingressNames {
-		manifest, err := r.k8sClient.ExtensionsV1beta1().Ingresses(namespace).Get(name, metav1.GetOptions{})
+		manifest, err := r.k8sClient.NetworkingV1beta1().Ingresses(namespace).Get(name, metav1.GetOptions{})
 		if apierrors.IsNotFound(err) {
 			r.logger.LogCtx(ctx, "level", "debug", "message", "did not find a ingress in the Kubernetes API")
 			// fall through
