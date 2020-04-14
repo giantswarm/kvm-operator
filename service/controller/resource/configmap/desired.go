f8274c1fb42f6ea0e763405728c4e68339f0b8d6
Zach Stone <zach@giantswarm.io>
2020-04-14 15:27:07 +0200

    Remove debug log

--- service/controller/resource/configmap/desired.go
+++ service/controller/resource/configmap/desired.go
@@ -68,7 +68,7 @@ func (r *Resource) newConfigMaps(customResource v1alpha1.KVMConfig) ([]*corev1.C
 	versions.KubernetesAPIHealthz = defaultVersions.KubernetesAPIHealthz
 	versions.KubernetesNetworkSetupDocker = defaultVersions.KubernetesNetworkSetupDocker
 	images := v_6_0_0.BuildImages(r.registryDomain, versions)
-	r.logger.Log("level", "debug", "message", fmt.Sprintf("images: %v#", images))
+
 	data := cloudconfig.IgnitionTemplateData{
 		CustomObject: customResource,
 		ClusterCerts: certs,
