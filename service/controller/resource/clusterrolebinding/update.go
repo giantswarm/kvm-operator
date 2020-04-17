6f106b252d0fb8144f089934a659ed331663795f
Zach Stone <zach@giantswarm.io>
2020-04-17 17:19:51 +0200

    Clean up logging

--- service/controller/resource/clusterrolebinding/update.go
+++ service/controller/resource/clusterrolebinding/update.go
@@ -22,37 +22,19 @@ func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange inte
 		// Create the cluster role bindings in the Kubernetes API.
 		for _, clusterRoleBinding := range clusterRoleBindingsToUpdate {
 			_, err := r.k8sClient.RbacV1().ClusterRoleBindings().Update(clusterRoleBinding)
-			r.logger.Log("level", "debug", "message", fmt.Sprintf(" got error: %v#", err))
-			r.logger.Log("level", "debug", "message", fmt.Sprintf(" error(): %v#", err.Error()))
 			if isExternalFieldImmutableError(err) {
-				// Create new CRB and delete the old one
-				r.logger.Log("level", "debug", "message", "Should re-create CRB")
-
-				// Simply create this as a new CRB
+				// We can't change a RoleRef, so create a new CRB which references the new RoleRef
 				newCRB := apiv1.ClusterRoleBinding{}
+
 				clusterRoleBinding.DeepCopyInto(&newCRB)
 				newCRB.SetName(clusterRoleBinding.Name + "-upgrading")
 
-				r.logger.Log("level", "debug", "message", "Creating new CRB instead of updating")
-				r.logger.Log("level", "debug", "message", fmt.Sprintf("Creating CRB: %v#", newCRB))
 				_, err := r.k8sClient.RbacV1().ClusterRoleBindings().Create(&newCRB)
 				if apierrors.IsAlreadyExists(err) {
 				} else if err != nil {
-					r.logger.Log("level", "debug", "message", "This error is from creation")
 					return microerror.Mask(err)
 				}
-
-				// Get existing CRB
-
-				// Change its name
-
-				// Create the new one using the original name
-
-				// Delete the renamed one. Wait for all old Pods to be deleted?
-
-				// Alternatively, create the new one with a new name
 			} else if err != nil {
-				r.logger.Log("level", "debug", "message", "This error is not an immutable error")
 				return microerror.Mask(err)
 			}
 		}
