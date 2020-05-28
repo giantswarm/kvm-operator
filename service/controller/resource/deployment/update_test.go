b349467c66ae721e2790b3795b4eefb9666bb2c0
Zach Stone <zach@giantswarm.io>
2020-05-28 13:51:09 +0200

    Extract TC client creation from update logic and pass fake TC client during update test

--- service/controller/resource/deployment/update_test.go
+++ service/controller/resource/deployment/update_test.go
@@ -1228,17 +1228,11 @@ func Test_Resource_Deployment_newUpdateChange(t *testing.T) {
 	var err error
 	var newResource *Resource
 	{
-		// fakeTC, err := tenantcluster.New(tenantcluster.Config{})
-		// if err != nil {
-		// 	t.Fatalf("can't create fake tenantcluster client: %#v", err)
-		// }
-		// fakeTC := FakeTenantClusterInterface{}
 		resourceConfig := Config{
 			DNSServers: "dnsserver1,dnsserver2",
 			G8sClient:  clientset,
 			K8sClient:  fake.NewSimpleClientset(),
 			Logger:     microloggertest.New(),
-			// TenantCluster: fakeTC,
 		}
 		newResource, err = New(resourceConfig)
 		if err != nil {
@@ -1246,8 +1240,10 @@ func Test_Resource_Deployment_newUpdateChange(t *testing.T) {
 		}
 	}
 
+	tenantK8sClient := fake.NewSimpleClientset()
+
 	for _, tc := range testCases {
-		updateState, err := newResource.newUpdateChange(tc.Ctx, tc.Obj, tc.CurrentState, tc.DesiredState)
+		updateState, err := newResource.updateDeployments(tc.Ctx, tc.CurrentState, tc.DesiredState, tenantK8sClient)
 		if err != nil {
 			t.Fatalf("expected %#v got %#v", nil, err)
 		}
@@ -1269,10 +1265,3 @@ func Test_Resource_Deployment_newUpdateChange(t *testing.T) {
 		})
 	}
 }
-
-// type FakeTenantClusterInterface struct {
-// }
-
-// func (FakeTenantClusterInterface) NewRestConfig(ctx context.Context, s string, t string) (*rest.Config, error) {
-
-// }
