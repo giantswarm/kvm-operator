4f939d436b2da2de96a8fde3bc0b6e06e066592e
Thomas Fussell <thomas@fussell.io>
2021-02-04 13:15:29 -0500

    add debug logging

--- service/controller/resource/pod/create.go
+++ service/controller/resource/pod/create.go
@@ -16,22 +16,24 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
+	r.logger.Debugf(ctx, "determining readiness for node pod")
+
 	var nodeIP string
 	var serviceName string
 	{
 		var ok bool
 		nodeIP, ok = pod.Annotations[key.AnnotationIp]
 		if !ok || nodeIP == "" {
-			r.logger.Debugf(ctx, "node pod %s/%s has no ip annotation %#q, skipping", pod.Namespace, pod.Name, key.AnnotationIp)
+			r.logger.Debugf(ctx, "node pod has no ip annotation %#q, skipping", key.AnnotationIp)
 			return nil
 		}
 
 		serviceName, ok = pod.Annotations[key.AnnotationService]
 		if !ok || serviceName == "" {
-			r.logger.Debugf(ctx, "node pod %s/%s has no service annotation %#q, skipping", pod.Namespace, pod.Name, key.AnnotationService)
+			r.logger.Debugf(ctx, "node pod has no service annotation %#q, skipping", key.AnnotationService)
 			return nil
 		} else if serviceName == key.MasterID {
-			r.logger.Debugf(ctx, "node pod %s/%s contains a workload cluster master node, skipping", pod.Namespace, pod.Name)
+			r.logger.Debugf(ctx, "node pod contains a workload cluster master node, skipping")
 			return nil
 		}
 	}
@@ -51,29 +53,49 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		}
 	}
 
+	if readyForTraffic {
+		r.logger.Debugf(ctx, "determined node pod is ready")
+	} else {
+		r.logger.Debugf(ctx, "determined node pod is not ready")
+	}
+
 	var endpoints *corev1.Endpoints
 	{
+		r.logger.Debugf(ctx, "retrieving endpoints %#q", serviceName)
 		var err error
 		endpoints, err = r.k8sClient.CoreV1().Endpoints(pod.Namespace).Get(ctx, serviceName, v1.GetOptions{})
 		if err != nil {
+			r.logger.Debugf(ctx, "error retrieving endpoints")
 			return microerror.Mask(err)
 		}
+		r.logger.Debugf(ctx, "retrieved endpoints")
 	}
 
 	var updated bool
 	addresses := endpoints.Subsets[0].Addresses
 	if readyForTraffic {
 		addresses, updated = addToEndpoints(addresses, nodeIP)
+		if updated {
+			r.logger.Debugf(ctx, "node ip %#q is not in endpoints but pod and node are ready, adding ip", nodeIP)
+		}
 	} else {
 		addresses, updated = removeFromEndpoints(addresses, nodeIP)
+		if updated {
+			r.logger.Debugf(ctx, "node ip %#q is in endpoints but pod or node is not ready, removing ip", nodeIP)
+		}
 	}
 
 	if updated {
+		r.logger.Debugf(ctx, "updating endpoints", pod.Namespace, pod.Name)
 		endpoints.Subsets[0].Addresses = addresses
 		_, err := r.k8sClient.CoreV1().Endpoints(pod.Namespace).Update(ctx, endpoints, v1.UpdateOptions{})
 		if err != nil {
+			r.logger.Debugf(ctx, "error updating endpoints", pod.Namespace, pod.Name)
 			return microerror.Mask(err)
 		}
+		r.logger.Debugf(ctx, "updated endpoints", pod.Namespace, pod.Name)
+	} else {
+		r.logger.Debugf(ctx, "not updating endpoints")
 	}
 
 	return nil
