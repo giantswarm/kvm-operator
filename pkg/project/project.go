1bd08748ac62d2b2c696d30038e877d7190b91a3
GitHub Action <action@github.com>
2021-02-11 23:12:21 +0000

    Bump version to 3.13.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "kvm-operator"
 	source      = "https://github.com/giantswarm/kvm-operator"
-	version     = "3.13.1"
+	version     = "3.13.2-dev"
 )
 
 func Description() string {
