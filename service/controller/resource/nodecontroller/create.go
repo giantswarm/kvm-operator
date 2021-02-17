24c9a017aafd4333ad4c70a020a1c0f992d541af
Thomas Fussell <thomas@fussell.io>
2021-02-17 10:18:03 -0400

    clean up

--- service/controller/resource/nodecontroller/create.go
+++ service/controller/resource/nodecontroller/create.go
@@ -7,11 +7,9 @@ import (
 	"time"
 
 	"github.com/giantswarm/errors/tenant"
-	"github.com/giantswarm/k8sclient/v5/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/tenantcluster/v4/pkg/tenantcluster"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/client-go/rest"
 
 	"github.com/giantswarm/kvm-operator/pkg/label"
 	"github.com/giantswarm/kvm-operator/pkg/project"
@@ -30,73 +28,41 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	clusterID := key.ClusterID(cr)
 	controllerKey := controllerMapKey(cr)
 
-	var restConfig *rest.Config
-	{
-		restConfig, err = r.workloadCluster.NewRestConfig(ctx, clusterID, key.ClusterAPIEndpoint(cr))
-		var shouldStop bool
-		if tenantcluster.IsTimeout(err) {
-			r.logger.Debugf(ctx, "waiting for certificates timed out")
-			shouldStop = true
-		} else if tenant.IsAPINotAvailable(err) || isServerError(err) || errors.Is(err, context.DeadlineExceeded) {
-			r.logger.Debugf(ctx, "tenant cluster is not available")
-			shouldStop = true
-		} else if err != nil {
-			return microerror.Mask(err)
-		}
-
-		if shouldStop {
-			r.controllerMutex.Lock()
-			if current, ok := r.controllers[controllerKey]; ok {
-				r.logger.Debugf(ctx, "stopping controller")
-				current.Stop()
-				delete(r.controllers, controllerKey)
-			}
-			r.controllerMutex.Unlock()
-			// Return early and wait for the next loop as there's no reason to watch an inaccessible Kubernetes API.
-			return nil
-		}
-	}
-
-	var k8sClient k8sclient.Interface
-	{
-		config := k8sclient.ClientsConfig{
-			Logger:     r.logger,
-			RestConfig: restConfig,
-		}
-		k8sClient, err = k8sclient.NewClients(config)
-		var shouldStop bool
-		if tenantcluster.IsTimeout(err) {
-			r.logger.Debugf(ctx, "waiting for certificates timed out")
-			shouldStop = true
-		} else if tenant.IsAPINotAvailable(err) || isServerError(err) || errors.Is(err, context.DeadlineExceeded) {
-			r.logger.Debugf(ctx, "tenant cluster is not available")
-			shouldStop = true
-		} else if err != nil {
-			return microerror.Mask(err)
-		}
-
-		if shouldStop {
-			r.controllerMutex.Lock()
-			if current, ok := r.controllers[controllerKey]; ok {
-				r.logger.Debugf(ctx, "stopping controller")
-				current.Stop()
-				delete(r.controllers, controllerKey)
-			}
-			r.controllerMutex.Unlock()
-			// Return early and wait for the next loop as there's no reason to watch an inaccessible Kubernetes API.
-			return nil
-		}
-	}
-
 	var desiredController *nodecontroller.Controller
 	{
+		k8sClient, err := key.CreateK8sClientForWorkloadCluster(ctx, cr, r.logger, r.workloadCluster)
+		var shouldStop bool
+		if tenantcluster.IsTimeout(err) {
+			r.logger.Debugf(ctx, "waiting for certificates timed out")
+			shouldStop = true
+		} else if tenant.IsAPINotAvailable(err) || isServerError(err) || errors.Is(err, context.DeadlineExceeded) {
+			r.logger.Debugf(ctx, "tenant cluster is not available")
+			shouldStop = true
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		if shouldStop {
+			r.controllerMutex.Lock()
+			if current, ok := r.controllers[controllerKey]; ok {
+				r.logger.Debugf(ctx, "stopping controller")
+				current.Stop()
+				delete(r.controllers, controllerKey)
+			}
+			r.controllerMutex.Unlock()
+			// Return early and wait for the next loop as there's no reason to watch an inaccessible Kubernetes API.
+			return nil
+		}
+
 		config := nodecontroller.Config{
 			Cluster:             cr,
 			ManagementK8sClient: r.k8sClient,
 			WorkloadK8sClient:   k8sClient,
 			Logger:              r.logger,
-			Name:                fmt.Sprintf("nodes-%s", clusterID),
+			Name:                fmt.Sprintf("%s-%s-nodes", project.Name(), clusterID),
 			Selector: labels.SelectorFromSet(map[string]string{
+				// When managing endpoints, we only consider node- and pod-readiness for workers. Master endpoint IPs
+				// are always present in the master endpoints object. This may change when we implement HA for KVM.
 				"role":                key.WorkerID,
 				label.OperatorVersion: project.Version(),
 			}),
