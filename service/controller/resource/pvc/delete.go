868743c72cb0580504d3215f6fd2b1ba79c8a965
Salvatore Mazzarino <dev@mazzarino.cz>
2021-05-27 15:08:45 +0200

    handle removal
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- service/controller/resource/pvc/delete.go
+++ service/controller/resource/pvc/delete.go
@@ -25,14 +25,47 @@ func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange inte
 	if len(pvcsToDelete) != 0 {
 		r.logger.Debugf(ctx, "deleting the PVCs in the Kubernetes API")
 
+		pvsList, err := r.k8sClient.CoreV1().PersistentVolumes().List(ctx, metav1.ListOptions{LabelSelector: LabelMountTag})
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
 		namespace := key.ClusterNamespace(customObject)
-		for _, PVC := range pvcsToDelete {
-			err := r.k8sClient.CoreV1().PersistentVolumeClaims(namespace).Delete(ctx, PVC.Name, metav1.DeleteOptions{})
+		for _, pvc := range pvcsToDelete {
+			err := r.k8sClient.CoreV1().PersistentVolumeClaims(namespace).Delete(ctx, pvc.Name, metav1.DeleteOptions{})
 			if apierrors.IsNotFound(err) {
 				// fall through
 			} else if err != nil {
 				return microerror.Mask(err)
 			}
+
+			// in case of local storage we need to manually remove the claim
+			storageClassName := *pvc.Spec.StorageClassName
+			if storageClassName != LocalStorageClass {
+				continue
+			}
+
+			var boundPV *corev1.PersistentVolume
+			for _, pv := range pvsList.Items {
+				ref := pv.Spec.ClaimRef
+				if ref.Name != pvc.Name && ref.Namespace != namespace {
+					continue
+				}
+
+				boundPV = pv.DeepCopy()
+			}
+
+			if boundPV == nil || boundPV.Spec.ClaimRef == nil {
+				continue
+			}
+
+			boundPV.Spec.ClaimRef = nil
+			_, err = r.k8sClient.CoreV1().PersistentVolumes().Update(ctx, boundPV, metav1.UpdateOptions{})
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			r.logger.Debugf(ctx, "unbound PV %s from pvc %s", boundPV.Name, pvc.Name)
 		}
 
 		r.logger.Debugf(ctx, "deleted the PVCs in the Kubernetes API")
